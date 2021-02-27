8a660c0f890fbfc615d2d937793489c31e344bf4
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-02-26 19:02:29 -0800

    Add input function to script.

--- create_local_admin_on_windows.ps1
+++ create_local_admin_on_windows.ps1
@@ -3,9 +3,9 @@
 # create local admin on Windows
 
 # declare variables
-$LocalAdmin = # Example: 'Foo.Bar'
-$Password = # Example: 'Password1234'
-$Description = # Example: 'Local Admin'
+$LocalAdmin = Read-Host -Prompt "What is the admin name? (Example: 'Foo.Bar')"
+$Password = Read-Host -Prompt "What is the password? (Example: 'Password1234')"
+$Description = Read-Host -Prompt "What is the description? (Example: 'Local Admin')"
 
 # create new user and set password
 New-LocalUser "$LocalAdmin" -Password $Password -FullName "$LocalAdmin" -Description "$Description"
