080f4f2f0c684f216b9f0d375fbf8d45555b2535
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-04 18:47:55 -0800

    Add input function to script.

--- create_standarduser_on_windows.ps1
+++ create_standarduser_on_windows.ps1
@@ -3,8 +3,9 @@
 # create standard user on Windows
 
 # declare variables
-$StandardUser = # Example: 'Standard User'
-$Description = # Example: 'Standard User'
+$StandardUser = Read-Host -Prompt "What is the standard user name? (Example: Standard.User)"
+$Password = Read-Host -Prompt "What is the password? (Example: Password1234)"
+$Description = Read-Host -Prompt "What is the description? (Example: Standard User)"
 
 # create new user and set password
 New-LocalUser $StandardUser -Password $Password -FullName $StandardUser -Description $Description
