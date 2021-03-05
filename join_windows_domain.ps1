5c0b249cdd3fe47a2cf7a95738fa6723320d6d59
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-05 06:35:57 -0800

    Add input function to script.

--- join_windows_domain.ps1
+++ join_windows_domain.ps1
@@ -3,8 +3,8 @@
 # join Windows domain
 
 # declare variables
-$DomainName = # Example:'domain.com'
-$ADAdmin = # Example: 'DomainAdmin'
+$DomainName = Read-Host -Prompt "What domain do you want to join? (Example: domain.com"
+$ADAdmin = Read-Host -Prompt "Example: DomainAdmin"
 
 # join computer to domain
 Add-Computer –DomainName $DomainName -Credential $ADAdmin -Restart –Force
