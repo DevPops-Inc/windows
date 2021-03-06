d812224a38080ff61cc2ffc4d345859a509bc648
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-06 07:22:40 -0800

    Addd input function to script.

--- open_photo_in_paint3d_on_windows.ps1
+++ open_photo_in_paint3d_on_windows.ps1
@@ -3,7 +3,7 @@
 # open photo in Paint 3D on Windows
 
 # declare variable and set it to file location
-$FileLocation = "C:\Users\adminvictor\Desktop\deltatre 2019.jpg"
+$FileLocation = Read-Host -Prompt "What is location of the file? (Example: C:\Users\adminvictor\Desktop\deltatre 2019.jpg)"
 
 # open file with Paint 3D
 Start-Process -FilePath "mspaint" -ArgumentList """$FileLocation /ForceBootstrapPaint3D"""
