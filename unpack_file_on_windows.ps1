b03bd61a3d7a45ee56d4594d577a864783d1a9e5
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-07 14:38:22 -0800

    Add input function to script.

--- unpack_file_on_windows.ps1
+++ unpack_file_on_windows.ps1
@@ -3,8 +3,8 @@
 # unpack file on Windows 
 
 #declare variables
-$SourceFilePath = # Example: 'C:\'
-$DestinationPath = # Example: 'C:\'
+$SourceFilePath = Read-Host -Prompt "Example: C:\vic.zip"
+$DestinationPath = Read-Host -Prompt "Example: C:\"
 
 #unpack zip file
 Expand-Archive -LiteralPath $SourceFilePath -DestinationPath $DestinationPath
