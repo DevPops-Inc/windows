94ab27aa8710964ceb960170bf39c62f29057295
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-08 18:41:20 -0800

    Uplaod disable services script.

---
+++ disable_service_on_startup_on_windows.ps1
@@ -0,0 +1,12 @@
+#!/bin/powershell
+
+# disable service on startup on Windows
+
+# get list of services
+Get-Service 
+
+# declare variables 
+$ServiceName = Read-Host -Prompt "What service would you like to dsiable on startup?"
+
+# set service to startup automatically
+Set-Service $ServiceName -StartupType Disabled
