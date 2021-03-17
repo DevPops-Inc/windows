4fa1e30ff0b42fd4bb176fc2942802c27705f53c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-17 05:51:35 -0700

    Fixed comments.

--- set_password_to_never_expire_on_activedirectory.ps1
+++ set_password_to_never_expire_on_activedirectory.ps1
@@ -2,8 +2,8 @@
 
 # set password to never expire on Active Directory
 
-#declare variables
+# declare variables
 $ADUser = Read-Host -Prompt "Which user's password do you wish to set to never expire? (Example: AD.User)"
 
-#set password for new user to never expire
+# set password for new user to never expire
 Set-LocalUser -Name "$ADUser" -PasswordNeverExpires 1
