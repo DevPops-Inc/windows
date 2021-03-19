008b68e411f6a0eab3b0fb4030f33768af400705
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-18 19:21:07 -0700

    Fixed comments.

--- unpack_file_on_windows.ps1
+++ unpack_file_on_windows.ps1
@@ -2,9 +2,9 @@
 
 # unpack file on Windows 
 
-#declare variables
+# declare variables
 $SourceFilePath = Read-Host -Prompt "Example: C:\vic.zip"
 $DestinationPath = Read-Host -Prompt "Example: C:\"
 
-#unpack zip file
+# unpack zip file
 Expand-Archive -LiteralPath $SourceFilePath -DestinationPath $DestinationPath
