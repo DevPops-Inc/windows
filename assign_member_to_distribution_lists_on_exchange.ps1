950d69adeafbf8c1a6996c82909a7ae632d8898a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-05 05:44:32 -0700

    Fixed user input.

--- assign_member_to_distribution_lists_on_exchange.ps1
+++ assign_member_to_distribution_lists_on_exchange.ps1
@@ -2,9 +2,13 @@
 
 # assign member to distribution list on Exchange
 
+# prompt user input 
+Write-Host "`nAssign member to distribution list on Exchange.`n"
+Pause
+
 # declare variables
-$email = Read-Host -Prompt "What email would you like to assign to the distribution list? (Example: email@domain.com)"
-$DistributionList = Read-Host -Prompt "What distribution list would you like to add the email to? (Example: group@domain.com)"
+$email = Read-Host -Prompt "`nWhat email would you like to assign to the distribution list? (Example: email@domain.com)`n"
+$DistributionList = Read-Host -Prompt "`nWhat distribution list would you like to add the email to? (Example: group@domain.com)`n"
 
 # assign member to distribution list
 Add-DistributionGroupMember -Identity $DistributionList -Member $email
