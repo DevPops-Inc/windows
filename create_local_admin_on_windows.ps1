32d682c9f4176a0ab78b0cc230b8de8ad027f796
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-06 05:53:43 -0700

    Added user input and fixed comment.

--- create_local_admin_on_windows.ps1
+++ create_local_admin_on_windows.ps1
@@ -2,10 +2,14 @@
 
 # create local admin on Windows
 
-# declare variables
-$LocalAdmin = Read-Host -Prompt "What is the admin name? (Example: 'Foo.Bar')"
-$Password = Read-Host -Prompt "What is the password? (Example: 'Password1234')"
-$Description = Read-Host -Prompt "What is the description? (Example: 'Local Admin')"
+# prompt user input 
+Write-Host "`nCreate local admin on Windows.`n"
+Pause
+
+# declare LocalAdmin, Password, and Description variables
+$LocalAdmin = Read-Host -Prompt "`nWhat is the admin name? (Example: 'Foo.Bar')`n"
+$Password = Read-Host -Prompt "`nWhat is the password? (Example: 'Password1234')`n"
+$Description = Read-Host -Prompt "`nWhat is the description? (Example: 'Local Admin')`n"
 
 # create new user and set password
 New-LocalUser "$LocalAdmin" -Password $Password -FullName "$LocalAdmin" -Description "$Description"
