0916726eae7010af25de8bdec2116a731f89f3da
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-06 21:57:13 -0700

    Added user input and fixed prompts.

--- create_standarduser_on_windows.ps1
+++ create_standarduser_on_windows.ps1
@@ -2,10 +2,14 @@
 
 # create standard user on Windows
 
-# declare variables
-$StandardUser = Read-Host -Prompt "What is the standard user name? (Example: Standard.User)"
-$Password = Read-Host -Prompt "What is the password? (Example: Password1234)"
-$Description = Read-Host -Prompt "What is the description? (Example: Standard User)"
+# prompt user input
+Read-Host -Prompt "`nCreate standard user on Windows.`n"
+Pause 
+
+# declare StandardUser, Password, and Description variables
+$StandardUser = Read-Host -Prompt "`nWhat is the standard user name? (Example: Standard.User)`n"
+$Password = Read-Host -Prompt "`nWhat is the password? (Example: Password1234)`n"
+$Description = Read-Host -Prompt "`nWhat is the description? (Example: Standard User)`n"
 
 # create new user and set password
 New-LocalUser $StandardUser -Password $Password -FullName $StandardUser -Description $Description
