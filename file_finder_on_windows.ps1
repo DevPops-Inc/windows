b6effcb316e4a4dcc8251dda9d06588599685e74
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-07 05:58:30 -0700

    Removed header and added comments.

--- file_finder_on_windows.ps1
+++ file_finder_on_windows.ps1
@@ -1,9 +1,13 @@
-#!/bin/powershell
-
 # file finder on Windows
 
+# prompt user input
+Write-Host "`nFile finder on Windows.`n"
+Pause
+
+# declare Filename and Drive variables
 $Filename = Read-Host -Prompt "What is the name of the file you're looked for? (Example: devops)"
 $Drive = Read-Host -Prompt "What drive do you think the file is on? (Example: C:\)"
 
+# find the file
 Write-Host "Please wait since this may this a while."
 Get-ChildItem -Path $Drive -Name *$Filename* -Recurse -Force
