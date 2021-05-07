8e6fc1837581de734296ce615264bab2ba008758
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-06 21:59:27 -0700

    Added user input and updated prompt.

--- disable_service_on_startup_on_windows.ps1
+++ disable_service_on_startup_on_windows.ps1
@@ -2,11 +2,15 @@
 
 # disable service on startup on Windows
 
+# prompt user input
+Write-Host "`nDisable server on startup on Windows."
+Pause 
+
 # get list of services
 Get-Service 
 
 # declare variables 
-$ServiceName = Read-Host -Prompt "What service would you like to disable on startup?"
+$ServiceName = Read-Host -Prompt "`nWhat service would you like to disable on startup?`n"
 
 # set service to startup automatically
 Set-Service $ServiceName -StartupType Disabled
