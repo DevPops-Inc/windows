848f916e2d2ac9d9fc363298874aaa2cfe6fb465
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 09:11:27 -0700

    Added user input.

--- map_drive_on_windows.ps1
+++ map_drive_on_windows.ps1
@@ -1,10 +1,12 @@
-#!/bin/powershell
-
 # map drive on Windows
 
+# prompt user input
+Write-Host "`nMap drive on Windows."
+Pause
+
 # declare variables
-$DriveLetter = Read-Host -Prompt "What letter would you like to map the drive with? (Example: D)"
-$PathWay = Read-Host -Prompt "What is the pathway of the drive you wish to map? (Example: \\Network\Share)"
+$DriveLetter = Read-Host -Prompt "`nWhat letter would you like to map the drive with? (Example: D)`n"
+$PathWay = Read-Host -Prompt "`nWhat is the pathway of the drive you wish to map? (Example: \\Network\Share)`n"
 
 # map drive
 New-PSDrive -Name "$DriveLetter" -PSProvider FileSystem -Root "$PathWay" -Persist
