ff3541b3245fb087219282d34e5b0565f06b94e6
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 09:18:17 -0700

    Removed header and added user input.

--- open_photo_in_paint3d_on_windows.ps1
+++ open_photo_in_paint3d_on_windows.ps1
@@ -1,9 +1,11 @@
-#!/bin/powershell 
-
 # open photo in Paint 3D on Windows
 
-# declare variable and set it to file location
-$FileLocation = Read-Host -Prompt "What is location of the file? (Example: C:\Users\adminvictor\Desktop\deltatre 2019.jpg)"
+# prompt user input 
+Write-Host "`nOpen photo in Paint 3D on Windows."
+Pause
+
+# declare FileLocation variable and set it to file location
+$FileLocation = Read-Host -Prompt "`nWhat is location of the file? (Example: C:\Users\adminvictor\Desktop\deltatre 2019.jpg)`n"
 
 # open file with Paint 3D
 Start-Process -FilePath "mspaint" -ArgumentList """$FileLocation /ForceBootstrapPaint3D"""
