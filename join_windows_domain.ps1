e6cdc065deba5c83ad6c6bfc338ae6602b75c23a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 07:39:24 -0700

    Fixed user prompts and updated variable.

--- join_windows_domain.ps1
+++ join_windows_domain.ps1
@@ -1,10 +1,12 @@
-#!/bin/powershell
-
 # join Windows domain
 
+# prompt user input
+Write-Host "`nJoin Windows domain."
+Pause
+
 # declare variables
-$DomainName = Read-Host -Prompt "What domain do you want to join? (Example: domain.com"
-$ADAdmin = Read-Host -Prompt "Example: DomainAdmin"
+$DomainName = Read-Host -Prompt "`nWhat domain do you want to join? (Example: domain.com)`n"
+$DomainAdmin = Read-Host -Prompt "`nWhat is your domain admin account? Example: DomainAdmin)`n"
 
 # join computer to domain
-Add-Computer –DomainName $DomainName -Credential $ADAdmin -Restart –Force
+Add-Computer –DomainName $DomainName -Credential $DomainAdmin -Restart –Force
