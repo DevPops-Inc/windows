ba6bde872df435ce0a5f8b2f65c346b194693d5a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 09:58:45 -0700

    Upload activate Windows script.

---
+++ activate_windows.ps1
@@ -0,0 +1,14 @@
+# activate Windows in PowerShell
+
+# prompt user input 
+Write-Host "`nActivate Windows in PowerShell"
+Pause
+
+# declare computer, key, and service variables variables 
+$computer = Get-Content $env:Computername
+$key = Read-Host -Prompt "`n What is your Windows key? (Exampel: aaaaa-bbbbb-ccccc-ddddd-eeeee)`n"
+$service = Get-WmiObject -query "select * from SoftwareLicensingService" -Computername $computer
+
+# active Windows 
+$service.InstallProductKey($key)
+$service.RefreshLicenseStatus()
