c5a5d3f7f41399a9675c2b8e2d0ef8f7ed36670b
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 09:15:08 -0700

    Added user input and fixed comment.

--- set_password_to_never_expire_on_activedirectory.ps1
+++ set_password_to_never_expire_on_activedirectory.ps1
@@ -1,9 +1,11 @@
-#!/bin/powershell
-
 # set password to never expire on Active Directory
 
-# declare variables
-$ADUser = Read-Host -Prompt "Which user's password do you wish to set to never expire? (Example: AD.User)"
+# prompt user input 
+Write-Host "`nSet password to never expire on Active Directory."
+Pause
+
+# declare ADUser variable
+$ADUser = Read-Host -Prompt "`nWhich user's password do you wish to set to never expire? (Example: AD.User)`n"
 
 # set password for new user to never expire
 Set-LocalUser -Name "$ADUser" -PasswordNeverExpires 1
