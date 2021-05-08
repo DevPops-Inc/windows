fd5c69d947050718b29d73924514ad4bfcf7d4c2
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 09:08:38 -0700

    Replaced Batch syntax with PowerShell syntax.

--- export_systeminfo_as_text_on_desktop_on_windows.ps1
+++ export_systeminfo_as_text_on_desktop_on_windows.ps1
@@ -1,5 +1,7 @@
-#!/bin/powershell
-
 # export system info to text file on desktop
 
-systeminfo > ~\desktop\systeminfo.txt
+# prompt user input
+Write-Host "`nExport system info to text file on desktop on Windows."
+Pause
+
+systeminfo | Out-File C:\Users\$env:username\Desktop\systeminfo.txt
