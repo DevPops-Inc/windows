614b971dc9de2917de9930f330470766f12a7992
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 09:36:40 -0700

    Added Get-ChildItem at the end of script.

--- unpack_file_on_windows.ps1
+++ unpack_file_on_windows.ps1
@@ -1,10 +1,16 @@
-#!/bin/powershell
-
 # unpack file on Windows 
 
+# prompt user input
+Write-Host "`nUnpack file on Windows."
+Pause
+
 # declare variables
-$SourceFilePath = Read-Host -Prompt "Example: C:\vic.zip"
-$DestinationPath = Read-Host -Prompt "Example: C:\"
+$sourceFilePath = Read-Host -Prompt "What is the filepath of the file you wish to unpack? (Example: C:\vic.zip)`n"
+$destinationPath = Read-Host -Prompt "What is the filepath you want the contents of the file to go? (Example: C:\)`n"
 
 # unpack zip file
-Expand-Archive -LiteralPath $SourceFilePath -DestinationPath $DestinationPath
+Expand-Archive -LiteralPath $sourceFilePath -destinationPath $destinationPath
+
+# change directory to destination and show contents
+Set-Location -Path $destinationPath
+Get-ChildItem
