95d158fecf32e383296d17a1eaf2a1d087611bb6
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-08 07:26:02 -0700

    Revised in main() function format.

--- install_openssh_on_windows.ps1
+++ install_openssh_on_windows.ps1
@@ -1,33 +1,49 @@
-#!/bin/powershell
-
 # install OpenSSH on Windows
 
-# PowerShell has to be version 6 and up
-# PowerShell must be ran in Administrator mode: 
-# Start-Process Powershell -Verb runAs
+# prompt user input
+Write-Host "`nInstall OpenSSH on Windows.`nPowerShell has to be version 6 and up.`nPowerShell must be ran in Administrator mode: Start-Process Powershell -Verb runAs`n"
+Pause
 
-# get OpenSSH status
-Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
-
-# install OpenSSH Client
-Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
-
-# install OpenSSH Server
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+# install OpenSSH Client and OpenSSH Server
+function InstallOpenSshClientAndServer()
+{
+    Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+}
 
 # set OpenSSH services to automatically start up
-Set-Service ssh-agent -StartupType Automatic
-Set-Service sshd -StartupType Automatic
+function AutoStartOpehSshServices()
+{
+    Set-Service ssh-agent -StartupType Automatic
+    Set-Service sshd -StartupType Automatic
+}
 
 # start up OpenSSH services
-Start-Service ssh-agent
-Start-Service sshd
+function StartOpenSshServices()
+{
+    Start-Service ssh-agent
+    Start-Service sshd
+}
 
-# get OpenSSH services statuses
-Get-Service "*ssh*"
+function InstallOpenSshOnWindows()
+{
+    # get OpenSSH status
+    Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
+    
+    # call previous functions 
+    InstallOpenSshClientAndServer
+    AutoStartOpehSshServices
+    StartOpenSshServices
 
-# get IP address
-ipconfig
+    # get OpenSSH services statuses
+    Get-Service "*ssh*"
 
-# get hostname and user accounts
-net user
+    # get IP address
+    ipconfig
+
+    # get hostname and user accounts
+    net user
+}
+
+# call main function 
+InstallOpenSshOnWindows
