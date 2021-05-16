b4ac89d8ac6d96c2e4e146e261b371b8593888db
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-16 10:24:14 -0700

    Upload SysAdmin scripts.

---
+++ get_printer_ports_on_windows.ps1
@@ -0,0 +1,8 @@
+# get printer ports on Windows
+
+# prompt user input 
+Write-Host "`nGet printer ports on Windows."
+Pause
+
+# get printer port
+Get-PrinterPort
---
+++ get_processes_on_windows.ps1
@@ -0,0 +1,8 @@
+# get processes on Windows
+
+# prompt user input 
+Write-Host "`nGet processes on Windows."
+Pause
+
+# get processes
+Get-Process
