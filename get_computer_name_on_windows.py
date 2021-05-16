12dbdf6b196a7eb8ebf6016f43d3ae4e0a1dd519
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-16 08:32:03 -0700

    Upload get comptuer name script.

---
+++ get_computer_name_on_windows.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+# get computer name on Windows 
+
+# import OS module 
+import os
+
+# prompt user input
+str(input"Get computer name on Windows.\nPress any key to continue.\n)
+
+# get computer name 
+os.system('hostname')
