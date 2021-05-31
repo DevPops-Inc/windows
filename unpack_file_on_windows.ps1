e7206e905bf54916df90c127b2dc62bd2f30387f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-31 07:47:16 -0700

    Added function.

--- unpack_file_on_windows.ps1
+++ unpack_file_on_windows.ps1
@@ -8,9 +8,16 @@ Pause
 $sourceFilePath = Read-Host -Prompt "What is the filepath of the file you wish to unpack? (Example: C:\vic.zip)`n"
 $destinationPath = Read-Host -Prompt "What is the filepath you want the contents of the file to go? (Example: C:\)`n"
 
-# unpack zip file
-Expand-Archive -LiteralPath $sourceFilePath -destinationPath $destinationPath
+# define UnpackFileOnWindows function
+function UnpackFileOnWindows()
+{
+    # unpack zip file
+    Expand-Archive -LiteralPath $sourceFilePath -destinationPath $destinationPath
 
-# change directory to destination and show contents
-Set-Location -Path $destinationPath
-Get-ChildItem
+    # change directory to destination and show contents
+    Set-Location -Path $destinationPath
+    Get-ChildItem
+}
+
+# call UnpackFileOnWindows function
+UnpackFileOnWindows
