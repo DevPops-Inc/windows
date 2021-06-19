0843a085982fc596dcffd348fe456ce53ea7d270
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-19 07:45:51 -0700

    Removed comments and added function.

--- activate_windows.ps1
+++ activate_windows.ps1
@@ -1,14 +1,18 @@
 # activate Windows in PowerShell
 
-# prompt user input 
 Write-Host "`nActivate Windows in PowerShell"
 Pause
 
-# declare computer, key, and service variables variables 
 $computer = Get-Content $env:Computername
-$key = Read-Host -Prompt "`n What is your Windows key? (Example: aaaaa-bbbbb-ccccc-ddddd-eeeee)`n"
+
+$key = Read-Host -Prompt "`n What is your Windows key? (Exampel: aaaaa-bbbbb-ccccc-ddddd-eeeee)`n"
+
 $service = Get-WmiObject -query "select * from SoftwareLicensingService" -Computername $computer
 
-# active Windows 
-$service.InstallProductKey($key)
-$service.RefreshLicenseStatus()
+function ActivateWindows()
+{
+    $service.InstallProductKey($key)
+    $service.RefreshLicenseStatus()
+}
+
+ActivateWindows
