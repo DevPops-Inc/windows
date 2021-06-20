d5adcfe56a455e76ab29de2f6df787c08733d30b
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-20 14:43:14 -0700

    Added parameters, get functions and logging.

--- ActivateWindows.ps1
+++ ActivateWindows.ps1
@@ -1,18 +1,52 @@
 # activate Windows in PowerShell
 
-Write-Host "`nActivate Windows in PowerShell"
-Pause
+[CmdletBinding()]
+param 
+(
+      [string] [Parameter(Mandatory = $False)] $computerName = ""
+    , [string] [Parameter(Mandatory = $False)] $licenseKey = ""
+)
 
-$computer = Get-Content $env:Computername
+function GetComputerName([string]$computerName)
+{
+    if (($computerName -eq $Null) -or ($computerName -eq ""))
+    {
+        $computerName = Read-Host -Prompt "`nPlease type the computer name (Example: Dev-PC) or press `"Ctrl`" and `"C`" keys to use the computer name from the environment: "
+        return $computerName
+    }
+    else 
+    {
+        $computerName = Get-Content $env:Computername
+        return $computerName
+    }
+}
 
-$key = Read-Host -Prompt "`n What is your Windows key? (Exampel: aaaaa-bbbbb-ccccc-ddddd-eeeee)`n"
-
-$service = Get-WmiObject -query "select * from SoftwareLicensingService" -Computername $computer
+function GetLicenseKey([string]$licenseKey)
+{
+    if (($licenseKey -eq $Null) -or ($licenseKey -eq ""))
+    {
+        $licenseKey = Read-Host -Prompt "`n What is your Windows key? (Example: aaaaa-bbbbb-ccccc-ddddd-eeeee)"
+        return $licenseKey
+    }
+    else
+    {
+        return $licenseKey
+    }
+}
 
 function ActivateWindows()
 {
-    $service.InstallProductKey($key)
+    Write-Host "`nActivate Windows in PowerShell`n"
+
+    GetComputerName $computerName
+    GetLicenseKey $licenseKey
+
+    $service = Get-WmiObject -query "select * from SoftwareLicensingService" -Computername $computerName
+
+    $service.InstallProductKey($licenseKey)
     $service.RefreshLicenseStatus()
+
+    Write-Host ("`nWindows has been activated on {0} using license key: {1}.`n" -F $computerName, $licenseKey)
 }
 
 ActivateWindows
