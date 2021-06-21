582c45dce083502389aefd7d9d112e48194ca9d5
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-21 08:45:17 -0700

    Deleted single line scripts.

--- check_disk_on_windows.ps1
+++
@@ -1,8 +0,0 @@
-# check disk on Windows
-
-# prompt user input
-Write-Host "`nCheck disk on Windows."
-Pause
-
-# check disk and repair C drive
-Write-Output y | chkdsk /f/r c:
--- check_systems_info_on_windows.ps1
+++
@@ -1,8 +0,0 @@
-# check systems info on Windows 
-
-# prompt user input
-Write-Host "`nCheck systems info on Windows."
-Pause
-
-# get systems info 
-systeminfo
