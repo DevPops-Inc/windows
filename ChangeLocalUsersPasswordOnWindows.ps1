6b28e56a34fc1103979d7fea905cbf15b1eebbdf
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-20 20:52:53 -0700

    Added parameters, get functions and logging.

--- ChangeLocalUsersPasswordOnWindows.ps1
+++ ChangeLocalUsersPasswordOnWindows.ps1
@@ -1,17 +1,47 @@
 # change local user's password on Windows
 
-Write-Host "`nChange local user's password on Windows.`n"
-Pause
+[CmdletBinding()]
+param (
+      [string][Parameter(Mandatory = $False)] $localUser = ""
+    , [string] [Parameter(Mandatory = $False)] $newPassword = ""
+)
 
-Write-Host "`nHere are the local users on this computer: `n"
-Get-LocalUser
-
-$localUser = Read-Host -Prompt "`nWho is the local user you would like to change the password for? (Example: LocalUser)`n"
-
-$newPassword = Read-Host -Prompt "`nWhat is the new password for the local user? (Example: Password123)`n"
-
-Function ChangeLocalUsersPassword() 
+function GetLocalUser([string]$localUser)
 {
+    if (($localUser -eq $Null) -or ($localUser -eq ""))
+    {
+        $localUser = Read-Host -Prompt "`nWho is the local user you would like to change the password for? (Example: LocalUser)"
+        return $localUser
+    }
+    else
+    {
+        return $localUser
+    }
+}
+
+function GetNewPassword($newPassword)
+{
+    if (($newPassword -eq $Null) -or ($newPassword -eq ""))
+    {
+        $newPassword = Read-Host -Prompt "`nWhat is the new password for the local user? (Example: Password123)"
+        return $newPassword
+    }
+    else
+    {
+        return $newPassword
+    }
+}
+
+function ChangeLocalUsersPassword() 
+{
+    Write-Host "`nChange local user's password on Windows.`n"
+
+    Write-Host "`nThe local users on this computer are: `n"
+    Get-LocalUser
+
+    GetLocalUser $localUser
+    GetNewPassword $newPassword
+
     Set-LocalUser -Name $localUser -Password (ConvertTo-SecureString -AsPlainText $newPassword -Force)
 
     Write-Host ("`nThe password for {0} has been changed.`n" -F $localUser)
