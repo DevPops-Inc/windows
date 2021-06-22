6eee4dabe136ee9bad11809432c34abd184eb257
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-22 08:46:07 -0700

    Updated user prompts.

--- ActivateWindows.ps1
+++ ActivateWindows.ps1
@@ -13,7 +13,7 @@ function GetComputerName([string]$computerName)
 {
     if (($computerName -eq $Null) -or ($computerName -eq ""))
     {
-        $computerName = Read-Host -Prompt "`nPlease type the computer name (Example: Dev-PC) or press `"Ctrl`" and `"C`" keys to use the computer name from the environment: "
+        $computerName = Read-Host -Prompt "`nPlease type the computer name (Example: Dev-PC) or press `"Ctrl`" and `"C`" keys to use the computer name from the environment"
         return $computerName
     }
     else 
@@ -27,7 +27,7 @@ function GetLicenseKey([string]$licenseKey)
 {
     if (($licenseKey -eq $Null) -or ($licenseKey -eq ""))
     {
-        $licenseKey = Read-Host -Prompt "`n What is your Windows key? (Example: aaaaa-bbbbb-ccccc-ddddd-eeeee)"
+        $licenseKey = Read-Host -Prompt "`nPlease type the Windows key (Example: aaaaa-bbbbb-ccccc-ddddd-eeeee)"
         return $licenseKey
     }
     else
@@ -50,11 +50,11 @@ function ActivateWindows([string]$computerName, [string]$licenseKey)
         $service.InstallProductKey($licenseKey)
         $service.RefreshLicenseStatus()
 
-        Write-Host ("`nWindows has been activated on {0} with license key: {1}.`n" -F $computerName, $licenseKey) -ForegroundColor Green
+        Write-Host ("`nWindows has been activated on {0} with license key: {1}" -F $computerName, $licenseKey) -ForegroundColor Green
     }
     catch
     {
-        Write-Host ("`nWindows failed to activate on {0} with license key: {1}`n" -F $computerName, $licenseKey) -ForegroundColor Red
+        Write-Host ("`nWindows failed to activate on {0} with license key: {1}" -F $computerName, $licenseKey) -ForegroundColor Red
     }
 }
 
