e0e726a8dec61844d0c7ef0ffe596969df5e0aa5
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-22 05:47:31 -0700

    Added try and catch conditions and colored logging.

--- ChangeLocalUsersPasswordOnWindows.ps1
+++ ChangeLocalUsersPasswordOnWindows.ps1
@@ -1,5 +1,7 @@
 # change local user's password on Windows
 
+# run this script with: .\ChangeLocalUsersPasswordOnWindows.ps1 -localUser < local user > -newPassword < new password >
+
 [CmdletBinding()]
 param (
       [string][Parameter(Mandatory = $False)] $localUser = ""
@@ -19,7 +21,7 @@ function GetLocalUser([string]$localUser)
     }
 }
 
-function GetNewPassword($newPassword)
+function GetNewPassword([string]$newPassword)
 {
     if (($newPassword -eq $Null) -or ($newPassword -eq ""))
     {
@@ -39,12 +41,19 @@ function ChangeLocalUsersPassword([string]$localUser, [string]$newPassword)
     Write-Host "`nThe local users on this computer are: `n"
     Get-LocalUser
 
-    GetLocalUser $localUser
-    GetNewPassword $newPassword
+    $localUser = GetLocalUser $localUser
+    $newPassword = GetNewPassword $newPassword
 
-    Set-LocalUser -Name $localUser -Password (ConvertTo-SecureString -AsPlainText $newPassword -Force)
+    try
+    {
+        Set-LocalUser -Name $localUser -Password (ConvertTo-SecureString -AsPlainText $newPassword -Force)
 
-    Write-Host ("`nThe password for {0} has been changed.`n" -F $localUser)
+        Write-Host ("`nThe password for {0} has been changed.`n" -F $localUser) -ForegroundColor Green
+    }
+    catch
+    {
+        Write-Host ("`nThe password for {0} failed to change.`n" -F $localUser) -ForegroundColor Red
+    }
 }
 
 ChangeLocalUsersPassword $localUser $newPassword
