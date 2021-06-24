f1dc08b0f4ef3241a1ebea9547e2dcb92196c2c9
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-23 18:57:31 -0700

    Added try and catch and error logging.

--- CheckDiskAndRestartWindows.ps1
+++ CheckDiskAndRestartWindows.ps1
@@ -4,12 +4,19 @@ function CheckDiskAndRestartWindows()
 {
     Write-Host "`nCheck disk and restart on Windows.`n"
 
-    Write-Output y | chkdsk /f/r c:
+    try
+    {
+        Write-Output y | chkdsk /f/r c:
 
-    Write-Host "`nPlease save your documents and close applications.`n"
-    Pause
-
-    Restart-Computer
+        Write-Host "`nPlease save your documents and close applications.`n"
+        Pause
+    
+        Restart-Computer
+    }
+    catch
+    {
+        Write-Host "`nCheck disk failed.`n" -ForegroundColor Red
+    }
 }
 
 CheckDiskAndRestartWindows
