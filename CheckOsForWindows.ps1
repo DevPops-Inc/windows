0c58cd73f80d1eb9178ba5cb42652ba744c2af59
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-27 10:40:58 -0700

    Added parameters, OS check, main function, try, catch, logging and renamed script.

--- CheckOsForWindows.ps1
+++ CheckOsForWindows.ps1
@@ -1,18 +1,20 @@
 function CheckOsForWindows()
 {
-    Write-Host "Checking operating system...`n"
+    Write-Host "`nChecking operating system..."
 
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
-        Write-Host "You are running this script on Windows.`n" -ForegroundColor Green
+        Write-Host "You are running this script on Windows." -ForegroundColor Green
     }
     else 
     {
         Write-Host "Your operating system is:" $hostOs
         
-        Write-Host "Sorry but this script only works on Windows.`n" -ForegroundColor Red
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system.`n"
 
         break
     }
