58855038f79f2a490bd619665e342720210b91cd
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-28 08:06:39 -0700

    Added try, catch, logging and renamed script.

---
+++ GetListOfDrives.ps1
@@ -0,0 +1,18 @@
+# get list of drives on Windows
+
+function GetListOfDrives()
+{
+    Write-Host "`nGet list of drives.`n"
+
+    try 
+    {
+        Write-Host "`nSuccessfully got list of drives on this computer.`n" -ForegroundColor Green
+        Get-PSDrive
+    }
+    catch 
+    {
+        Write-Host "`nFailed to get list of drives on this computer.`n" -Foreground Red
+    }
+}
+
+GetListOfDrives
