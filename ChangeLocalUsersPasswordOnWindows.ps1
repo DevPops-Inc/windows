f0a02cff45f140c615155b9ca9d6e691866d64e2
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-07-08 21:28:01 -0700

    Added OS check, fixed spacing in prompts and firevised logging.

--- ChangeLocalUsersPasswordOnWindows.ps1
+++ ChangeLocalUsersPasswordOnWindows.ps1
@@ -12,7 +12,8 @@ function GetLocalUser([string]$localUser)
 {
     if (($localUser -eq $Null) -or ($localUser -eq ""))
     {
-        $localUser = Read-Host -Prompt "`nPlease type the local user you would like to change the password for (Example: LocalUser)"
+        $localUser = Read-Host -Prompt "Please type the local user you would like to change the password for (Example: LocalUser)"
+
         return $localUser
     }
     else
@@ -25,7 +26,8 @@ function GetNewPassword([string]$newPassword)
 {
     if (($newPassword -eq $Null) -or ($newPassword -eq ""))
     {
-        $newPassword = Read-Host -Prompt "`nPlease type the new password for the local user (Example: Password123)"
+        $newPassword = Read-Host -Prompt "Please type the new password for the local user (Example: Password123)"
+
         return $newPassword
     }
     else
@@ -34,11 +36,33 @@ function GetNewPassword([string]$newPassword)
     }
 }
 
+function CheckOsForWindows()
+{
+    Write-Host "`nChecking operating system..."
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "You are running this script on Windows." -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "Your operating system is:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system.`n"
+        break
+    }
+    Write-Host "Finished checking operating system.`n"
+}
+
 function ChangeLocalUsersPassword([string]$localUser, [string]$newPassword) 
 {
     Write-Host "`nChange local user's password on Windows.`n"
+    CheckOsForWindows
 
-    Write-Host "`nThe local users on this computer are: `n"
+    Write-Host "`nThe local users on this computer are:"
     Get-LocalUser
 
     $localUser = GetLocalUser $localUser
@@ -48,11 +72,11 @@ function ChangeLocalUsersPassword([string]$localUser, [string]$newPassword)
     {
         Set-LocalUser -Name $localUser -Password (ConvertTo-SecureString -AsPlainText $newPassword -Force)
 
-        Write-Host ("`nThe password for {0} has been changed.`n" -F $localUser) -ForegroundColor Green
+        Write-Host ("Successfully changed the password for {0}." -F $localUser) -ForegroundColor Green
     }
     catch
     {
-        Write-Host ("`nThe password for {0} failed to change.`n" -F $localUser) -ForegroundColor Red
+        Write-Host ("Failed to change the password for {0}." -F $localUser) -ForegroundColor Red
     }
 }
 
