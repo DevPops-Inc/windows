884a1ced4a2a427298d288666eedcd157c934cfc
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-07-08 21:36:04 -0700

    Added OS check and fixed spacing in prompts.

--- CheckDiskOnWindows.ps1
+++ CheckDiskOnWindows.ps1
@@ -1,18 +1,39 @@
 # check disk on Windows
 
+function CheckOsForWindows()
+{
+    Write-Host "`nChecking operating system..."
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "You are running this script on Windows." -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "Your operating system is:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system.`n"
+        break
+    }
+    Write-Host "Finished checking operating system.`n"
+}
+
 function CheckDisk()
 {
+    Write-Host "`nCheck disk on Windows.`n"
+    CheckOsForWindows
+
     try
     {
-        # prompt user input
-        Write-Host "`nCheck disk on Windows.`n"
-
         # check disk and repair C drive
         Write-Output y | chkdsk /f/r c:
     }
     catch
     {
-        Write-Host "`nCheck disk failed.`n" -ForegroundColor Red
+        Write-Host "Check disk failed." -ForegroundColor Red
     }
 }
 
