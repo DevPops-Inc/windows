f86741538df771e94909ac370d151cf2a646a168
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-07-19 09:20:20 -0700

    Added parameter, OS check, get function, main function, exception handling, logging and renamed script.

---
+++ RemoveContactFromExchange.ps1
@@ -0,0 +1,64 @@
+# remove contact from Exchange
+
+# you can run this script with: .\RemoveContactFromExchange.ps1 -contactName < contact >
+
+[CmdletBinding()]
+param(
+    [string] [Parameter(Mandatory = $False)] $contactName = ""
+)
+
+function GetContactName([string]$contactName)
+{
+    if (($contactName -eq $Null) -or ($contactName -eq ""))
+    {
+        $contactName = Read-Host -Prompt "Please type the contact you wish to remove and press `"Enter`" key (Example: software.vendor)"
+
+        return $contactName
+    }
+    else
+    {
+        return $contactName
+    }
+}
+
+function CheckOsForWindows()
+{
+    Write-Host "`nChecking operating system..."
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "You are running this script on Windows." -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "Your operating system is:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system.`n"
+        break
+    }
+    Write-Host "Finished checking operating system.`n"
+}
+
+function RemoveContact([string]$contactName)
+{
+    Write-Host "`nRemove contact from Exchange.`n"
+    CheckOsForWindows
+
+    $contactName = GetContactName $contactName
+
+    try 
+    {
+        Remove-MailContact -Identity $contactName -force
+        
+        Write-Host ("Successfully removed {0} from Exchange." -F $contactName) -ForegroundColor Green
+    }
+    catch 
+    {
+        Write-Host ("Failed to remove {0} from Exchange." -F $contactName) -ForegroundColor Red
+    }
+}
+
+RemoveContact $contactName
