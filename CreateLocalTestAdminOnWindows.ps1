85b63eec6d6c423c1fb57e7e91a97432559a64ac
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-08-09 06:33:01 -0700

    Added parameter check, updated prompt, added OS check, execution time and updated error logging.

--- CreateLocalTestAdminOnWindows.ps1
+++ CreateLocalTestAdminOnWindows.ps1
@@ -4,16 +4,38 @@
 
 [CmdletBinding()]
 param(
-      [string] [Parameter(Mandatory = $False)] $testAdmin = "TestAdmin"
-    , [string] [Parameter(Mandatory = $False)] $testAdminPassword = ""
-    , [string] [Parameter(Mandatory = $False)] $testAdminDescription = "TestAdmin"
+      [string]       [Parameter(Mandatory = $False)] $testAdmin = "TestAdmin"
+    , [securestring] [Parameter(Mandatory = $False)] $testAdminPassword = $Null
+    , [string]        [Parameter(Mandatory = $False)] $testAdminDescription = "TestAdmin"
 )
+
+function CheckOsForWindows()
+{
+    Write-Host "`nChecking operating system..."
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system.`n"
+        break
+    }
+    Write-Host "Finished checking operating system.`n"
+}
 
 function GetTestAdminPassword()
 {
     if (($testAdminPassword -eq $Null) -or ($testAdminPassword -eq ""))
     {
-        $testAdminPassword = Read-Host -Prompt "`nPlease create password for TestAdmin (Example: Password123)`n"
+        $testAdminPassword = Read-Host -Prompt "Please create password for TestAdmin and press `"Enter`" key (Example: Password123)" -AsSecureString
+        
         return $testAdminPassword
     }
     else
@@ -22,36 +44,92 @@ function GetTestAdminPassword()
     }
 }
 
-function CreateTestAdmin([string]$testAdmin, [string]$testAdminPassword, [string]$testAdminDescription)
+function CheckParameters([string]$testAdmin, [securestring]$testAdminPassword, [string]$testAdminDescription)
 {
-    Write-Host "`nCreate local TestAdmin on Windows."
+    Write-Host "`nStarted checking parameters..."
+    $valid = $True
+
+    Write-Host "`nParameters:"
+    Write-Host "----------------------------------------"
+    Write-Host ("testAdmin           : {0}" -F $testAdmin)
+    Write-Host ("testAdminPassword   : {0}" -F "***")
+    Write-Host ("testAdminDescription: {0}" -F $testAdminDescription)
+    Write-Host "----------------------------------------"
+
+    if (($testAdmin -eq $Null) -or ($testAdmin -eq ""))
+    {
+        Write-Host "testAdmin is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if (($testAdminPassword -eq $Null) -or ($testAdminPassword -eq ""))
+    {
+        Write-Host "testAdminPassword is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if (($testAdminDescription -eq $Null) -or ($testAdminDescription -eq ""))
+    {
+        Write-Host "testAdminDescription is not set." -ForegroundColor Red
+        $valid = $True
+    }
+
+    Write-Host "Finished checking parameters."
+
+    if ($valid = $True)
+    {
+        Write-Host "All parameters checks passed." -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "One or more paramateres are incorrect, exiting script." -ForegroundColor Red
+        exit $result
+    }
+    
+    Write-Host ""
+}
+
+function CreateTestAdmin([string]$testAdmin, [securestring]$testAdminPassword, [string]$testAdminDescription)
+{
+    Write-Host "`nCreate local TestAdmin on Windows.`n"
+    CheckOsForWindows
+
+    $testAdminPassword = GetTestAdminPassword $testAdminPassword
+    CheckParameters $testAdmin $testAdminPassword $testAdminDescription
 
     try 
     {
-          # create testAdmin and set password
+        $startDateTime = (Get-Date)
+        Write-Host "Started creating testAdmin account at: " $startDateTime
+
         New-LocalUser "$testAdmin" -Password $testAdminPassword -FullName "$testAdmin" -Description "$testAdminDescription"
 
-        # add testAdmin as local administrator
         Add-LocalGroupMember -Group "Administrators" -Member "$testAdmin"
 
-        # set testAdmin password  to never expire
         Set-LocalUser -Name "$testAdmin" -PasswordNeverExpires 1
 
-        
-        Write-Host ("`n{0} account has been created.`n" -F $testAdmin) -ForegroundColor Green
+        Write-Host ("Successfully created {0} account." -F $testAdmin) -ForegroundColor Green
 
-        # check if testAdmin has been added
-        Write-Host "`nThe users on this computer are: `n"
+        Write-Host "The users on this computer are: "
         Get-LocalUser
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished creating testAdmin account at: " $finishedDateTime
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours, {1} minutes, {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
-        Write-Host ("`nFailed to create {0} account.`n" -F $testAdmin) -ForegroundColor Red
+        Write-Host ("Failed to create {0} account." -F $testAdmin) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
         
-        Write-Host "`nThe users on this computer are: `n"
+        Write-Host "The users on this computer are: "
         Get-LocalUser
     }
-  
 }
 
 CreateTestAdmin $testAdmin $testAdminPassword $testAdminDescription
