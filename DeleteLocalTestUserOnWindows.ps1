2f7c0ebe3b27a687d77c5e5d1fcf2e4452d48f6d
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-10 17:50:07 -0700

    Added OS check, parameter checks, execution time and better exception handling.

--- DeleteLocalTestUserOnWindows.ps1
+++ DeleteLocalTestUserOnWindows.ps1
@@ -7,11 +7,35 @@ param(
     [string] [Parameter(Mandatory = $False)] $testUser = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperationSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+
+        Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+}
+
 function GetTestUser([string]$testUser)
 {
     if (($testUser -eq $Null) -or ($testUser -eq ""))
     {
-        $testUser = Read-Host -Prompt "`nPlease input test username (Example: testuser)"
+        $testUser = Read-Host -Prompt "`nPlease input test username and press `"Enter`" key (Example: testuser)"
+
         return $testUser
     }
     else 
@@ -20,26 +44,69 @@ function GetTestUser([string]$testUser)
     }
 }
 
+function CheckParameters([string]$testUser)
+{
+    Write-Host "`nStarted checking parameters at" (Get-Date).DateTime
+    $valid = $True
+
+    Write-Host "`nParameters:"
+    Write-Host "----------------------------------------"
+    Write-Host ("testUser: {0}" -F $testUser)
+    Write-Host "----------------------------------------"
+
+    if (($testUser -eq $Null) -or ($testUser -eq ""))
+    {
+        Write-Host "testUser is not set." -ForegroundColor -Red
+        $valid = $False
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed.`n" -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor -Red
+
+        exit -1
+    }
+}
+
 function DeleteTestUser([string]$testUser)
 {
     Write-Host "`nDelete local test user on Windows`n"
+    CheckOsForWindows
 
     $testUser = GetTestUser $testUser
+    CheckParameters $testUser
 
     try
     {
-        # delete testUser
+        $startDateTime = (Get-Date)
+        Write-Host "Started deleting local test user at" $startDateTime
+
         Remove-LocalUser -Name "$testUser"
 
-        Write-Host ("`nTest user {0} successfully deleted.`n" -F $testUser) -ForegroundColor Green
+        Write-Host ("`nSuccessfuly deleted test user: {0}`n" -F $testUser) -ForegroundColor Green
 
         Write-Host "The users on this computer are: `n"
         Get-LocalUser
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished deleting local user at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch
     {
-        Write-Host ("`nTest user {0} failed to delete.`n" -F $testUser) -ForegroundColor Red
+        Write-Host ("`nFailed to delete test user: {0} `n" -F $testUser) -ForegroundColor Red
 
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
+        
         Write-Host "The users on this computer are: `n"
         Get-LocalUser
     }
