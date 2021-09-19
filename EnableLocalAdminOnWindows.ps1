a87d58e64d339a5f0de946cef57d0f92c12ef073
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-19 09:46:00 -0700

    Added OS check, execution time and better excpetion handling.

--- EnableLocalAdminOnWindows.ps1
+++ EnableLocalAdminOnWindows.ps1
@@ -1,18 +1,54 @@
 # enable local admin on Windows 
 
+function CheckOsForWindows()
+{
+    Write-Host "`nStarted checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+
+        Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+}
+
 function EnableLocalAdmin()
 {
     Write-Host "`nEnable local Administrator account on Windows.`n"
+    CheckOsForWindows
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started enabling local Administrator account at" $startDateTime
+
         Get-LocalUser -Name "Administrator" | Enable-LocalUser
 
         Write-Host "`nSuccessfully enabled local Administrator account." -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished enabling local Administrator account at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch
     {
         Write-Host "`nFailed to enable local Administrator account.`n" -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
