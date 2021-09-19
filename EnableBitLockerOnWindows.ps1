336b3e50a36ebdddecdf648f3d87b043dc528018
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-19 09:24:22 -0700

    Added OS check, updated prompts, added parameters check, execution time and better exception handling.

--- EnableBitLockerOnWindows.ps1
+++ EnableBitLockerOnWindows.ps1
@@ -7,11 +7,35 @@ param(
     [string] [Parameter(Mandatory = $False)] $driveLetter = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "`nStarted checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+
+        Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red`
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetDriveLetter([string]$driveLetter)
 {
     if (($driveLetter -eq $Null) -or ($driveLetter -eq ""))
     {
-        $driveLetter = Read-Host -Prompt "`nPlease type the drive letter you would like to enable BitLocker on (Example: C:)"
+        $driveLetter = Read-Host -Prompt "`nPlease type the drive letter you would like to enable BitLocker on and press `"Enter`" key (Example: C:)"
         
         return $driveLetter
     }
@@ -21,21 +45,66 @@ function GetDriveLetter([string]$driveLetter)
     }
 }
 
+function CheckParameters([string]$driveLetter)
+{
+    Write-Host "`nStarted checking parameters at" (Get-Date).DateTime
+    $valid = $True
+    
+    Write-Host "`nParameters:"
+    Write-Host "----------------------------------------"
+    Write-Host ("driveLetter: {0}" -F $driveLetter)
+    Write-Host "----------------------------------------"
+
+    if (($driveLetter -eq $Null) -or ($driveLetter -eq ""))
+    {
+        Write-Host "driveLetter is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed.`n" -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "One or more parameters are incorrect, exiting script."  -ForegroundColor Red
+
+        exit -1
+    }
+
+}
+
 function EnableBitLocker([string]$driveLetter)
 {
     Write-Host "`nEnable BitLocker on Windows.`n"
+    CheckOsForWindows
 
     $driveLetter = GetDriveLetter $driveLetter
+    CheckParameters $driveLetter
 
     try
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started enabling BitLocker at" $startDateTime
+
         Enable-BitLocker -MountPoint "$driveLetter"
 
         Write-Host ("`nSuccessfully enabled BitLocker on drive {0}.`n" -F $driveLetter) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished enabling BitLocker at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host ("`nFailed to enable BitLocker on drive {0}.`n" -F $driveLetter) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
