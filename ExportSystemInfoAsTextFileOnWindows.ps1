0528128da580e57397253935acf106b68f8ab6db
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-26 07:40:30 -0700

    Added OS check, updated prompts, added execution time and better exception handling.

--- ExportSystemInfoAsTextFileOnWindows.ps1
+++ ExportSystemInfoAsTextFileOnWindows.ps1
@@ -8,17 +8,40 @@ param(
     , [string] [Parameter(Mandatory = $False)] $fileDestination = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System: " (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+
+        Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetSystemInfoFile([string]$systemInfoFile)
 {
     if (($systemInfoFile -eq $Null) -or ($systemInfoFile -eq ""))
     {
-        $systemInfoFile = Read-Host -Prompt "`nPlease type filename for system info text file or press `"Ctrl`" and `"C`" keys to accept default filename: systeminfo.txt`n"
+        $systemInfoFile = Read-Host -Prompt "`nPlease type filename for system info text file and press `"Enter`" key (Example: systeminfo.txt)"
 
         return $systemInfoFile
     }
     else 
     {
-        $systemInfoFile = "systeminfo.txt"
         return $systemInfoFile
     }
 }
@@ -27,59 +50,86 @@ function GetFileDestination([string]$fileDestination)
 {
     if (($fileDestination -eq $Null) -or ($fileDestination -eq ""))
     {
-        $fileDestination = Read-Host -Prompt "`nPlease type file destination for system info file or press `"Ctrl`" and `"C`" keys to accept destination as desktop.`n"
+        $fileDestination = Read-Host -Prompt "`nPlease type file destination for system info file and press `"Enter`" key (Example: C:\Users\`$Env:Username\Desktop\)"
 
         return $fileDestination
     }
     else 
     {
-        $fileDestination = C:\Users\$Env:Username\Desktop\
-
         return $fileDestination
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$systemInfoFile, [string]$fileDestination)
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "`nStarted checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "`nParameters:"
+    Write-Host "-------------------------------------------"
+    Write-Host ("systemInfoFile : {0}" -F $systemInfoFile)
+    Write-Host ("fileDestination: {0}" -F $fileDestination)
+    Write-Host "-------------------------------------------"
 
-    if ($hostOs -eq "Win32NT")
+    if (($systemInfoFile -eq $Null) -or ($systemInfoFile -eq ""))
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "systemInfoFile is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if (($fileDestination -eq $Null) -or ($fileDestination -eq ""))
+    {
+        Write-Host "fileDestination is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed.`n" -ForegroundColor Green
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+        Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
-
-        break
+        exit -1
     }
-    Write-Host "Finished checking operating system.`n"
 }
 
 function ExportSystemInfoAsTextFile([string]$systemInfoFile, [string]$fileDestination)
 {
     Write-Host "`nExport system info to text file on Windows.`n"
-
     CheckOsForWindows
+
+    $systemInfoFile = GetSystemInfoFile $systemInfoFile
+    $fileDestination = GetFileDestination $fileDestination
+    CheckParameters $systemInfoFile $fileDestination
 
     try
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started exporting system info to text file at" $startDateTime
+
         systeminfo | Out-File $fileDestination + "\" $systemInfoFile
 
         Write-Host ("`nSuccessfully exported system info text file as {0} at this location: {1}`n" -F $systemInfoFile, $fileDestination) -ForegroundColor Green
 
         Get-ChildItem -Path $fileDestination
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished exporting systme info to text file at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch
     {
         Write-Host ("`nFailed to export system info text file as {0} at this location: {1}`n" -F $systemInfoFile, $fileDestination)
 
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
+        
         Get-ChildItem -Path $fileDestination
     }
 }
