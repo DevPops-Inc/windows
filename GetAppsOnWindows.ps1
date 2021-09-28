69e505686e8efd6c5e0fce7cb458dd2cfc772037
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-27 17:37:09 -0700

    Updated OS check, added execution time and updated exception handling.

--- GetAppsOnWindows.ps1
+++ GetAppsOnWindows.ps1
@@ -2,23 +2,26 @@
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
+        Write-Host "Operating System:" $hostOs
         
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
 }
 
 function GetApps()
@@ -28,14 +31,27 @@ function GetApps()
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started getting applications on Windows at" $startDateTime
+
         Write-Host "The applications on this computer are:"
         Get-ChildItem -Path "C:\Program Files*" -Name "*exe" -Recurse -Force
         
         Write-Host "Successfully got applications on Windows." -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        
+        Write-Host "Finished getting applications on Windows at" $finishedDateTime
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host "Failed to get applications on Windows." -ForegroundColor Red
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
