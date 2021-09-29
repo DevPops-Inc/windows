5632a8e5e2a4431cbbe1d8cae324dc853ec7d3f4
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-29 05:55:27 -0700

    Added OS check, rearranged main function, added execution time and updated exception handling.

--- GetListOfDrives.ps1
+++ GetListOfDrives.ps1
@@ -1,17 +1,49 @@
 # get list of drives
 
+function CheckOs()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs -ForegroundColor Green
+    }
+    Write-Host "Finished checking operating system at" (Get-Date).DateTime
+    Write-Host ""
+}
+
 function GetListOfDrives()
 {
     Write-Host "`nGet list of drives.`n"
+    CheckOs
 
     try 
     {
-        Write-Host "`nSuccessfully got list of drives on this computer.`n" -ForegroundColor Green
+        $startDateTime = (Get-Date)
+        Write-Host "Started getting list of drives at" $startDateTime
+
         Get-PSDrive
+
+        Write-Host "`nSuccessfully got list of drives on this computer" -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished getting list of drives at" $finishedDateTime
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host "`nFailed to get list of drives on this computer.`n" -Foreground Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
