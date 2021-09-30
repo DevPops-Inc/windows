6a0f5d0d4910e62b0b23069e2bd03ff0ad233fe4
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-30 08:39:23 -0700

    Updated OS check, added execution time and updated exception handling.

--- GetLocalUsersOnWindows.ps1
+++ GetLocalUsersOnWindows.ps1
@@ -2,44 +2,55 @@
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
-
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
+        Write-Host "Operating System:" $hostOs
         
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
-
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
 }
-
 
 function GetLocalUsers()
 {
     Write-Host "`nGet local users on Windows.`n" 
-
     CheckOsForWindows
     
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started getting local users at" $startDateTime
+
         Write-Host "The users on this computer are: "
         Get-LocalUser
 
         Write-Host "`nSuccessfully got local users on this computer." -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished getting local users at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch
     {
         Write-Host "`nFailed getting local users on this computer.`n" -ForegroundColor Red
+        
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
