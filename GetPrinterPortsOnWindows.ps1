3534a11b4b45f4c4f5164388039782afe6920728
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-01 05:49:03 -0700

    Updated OS check, added execution time, and updated exception handling.

--- GetPrinterPortsOnWindows.ps1
+++ GetPrinterPortsOnWindows.ps1
@@ -2,24 +2,26 @@
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
-
+    Write-Host "Start checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at:" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
+        Write-Host "Operating System:" $hostOs
         
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
 }
 
 function GetPrinterPortsOnWindows()
@@ -29,14 +31,23 @@ function GetPrinterPortsOnWindows()
 
     try
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started getting printer ports at" $startDateTime
+
         Write-Host "The printer ports on this computer are: "
         Get-PrinterPort
 
         Write-Host "Successfuly got printer ports on this computer.`n" -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished getting printer ports at" $finishedDateTime
     }
     catch
     {
         Write-Host "Failed to get printer ports on this computer.`n" -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
