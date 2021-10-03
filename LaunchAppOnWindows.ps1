18cdacd8dd482a5473c952243b412422c8574c34
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-03 06:27:01 -0700

    Updated OS check, added parameter checks, added Test-Path for parameter, execution time and updated exception handling.

--- LaunchAppOnWindows.ps1
+++ LaunchAppOnWindows.ps1
@@ -7,6 +7,30 @@ param(
     [string] [Parameter(Mandatory = $False)] $appName = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetAppName([string]$appName)
 {
     if (($appName -eq $Null) -or ($appName -eq ""))
@@ -21,25 +45,36 @@ function GetAppName([string]$appName)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$appName)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
+    Write-Host "`nParameters:"
+    Write-Host "---------------------------"
+    Write-Host ("appName: {0}" -F $appName)
+    Write-Host "---------------------------"
+
+    if (($appName -eq $Null) -or ($appName -eq "")) 
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "appName is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+        Write-Host "One or more parameter checks incorrect, exiting script." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
-        break
+        exit -1
     }
-    Write-Host "Finished checking operating system.`n"
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+
+    Write-Host ""
 }
 
 function LaunchApp([string]$appName)
@@ -51,16 +86,34 @@ function LaunchApp([string]$appName)
     Get-ChildItem -Path "C:\Program Files*" -Name "*exe" -Recurse -Force
 
     $appName = GetAppName $appName
+    CheckParameters $appName
 
     try
     {
+        $startDateTime = (Get-Date)
+        Write-Host ("Started launching {0} application at {1}" -F $appName, $startDateTime)
+
+        if ((Test-Path $appName) -eq $False)
+        {
+            throw ("{0} does not exist" -F $appName)
+        }
+
         Start-Process -FilePath $appName
         
         Write-Host ("Successfully launched {0} application." -F $appName) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host ("Finished launching {0} application at {1}" -F $appName, $finishedDateTime)
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host ("Failed to launch {0} application." -F $appName) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
