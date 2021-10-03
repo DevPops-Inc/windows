b077f7fd2c1603fd9f487929a52ab6dd8326a45f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-02 18:40:59 -0700

    Updated OS check, added logging to functions, added execution time and updated exception handling.

--- InstallOpenSshOnWindows.ps1
+++ InstallOpenSshOnWindows.ps1
@@ -2,42 +2,65 @@
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
-
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
+        Write-Host "Operating System:" $hostOs
         
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
 }
 
 function InstallOpenSshClientAndServer()
 {
+    Write-Host "Started installing OpenSSH client at" (Get-Date).DateTime
+    
     Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+    
+    Write-Host "Finished installing OpenSSH client at" (Get-Date).DateTime
+    Write-Host ""
+
+    Write-Host "Started installing OpenSSH server at" (Get-Date).DateTime
+    
     Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    
+    Write-host "Finished install OpenSSH server at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function AutoStartOpehSshServices()
 {
+    Write-Host "Started setting OpenSSH services to autostart at" (Get-Date).DateTime
+
     Set-Service ssh-agent -StartupType Automatic
     Set-Service sshd -StartupType Automatic
+
+    Write-Host "Finished setting OpenSSH servers to autostart at" (Get-Date).DateTime
+
+    Write-Host ""
 }
 
 function StartOpenSshServices()
 {
+    Write-Host "Starting OpenSSH services at" (Get-Date).DateTime
+    
     Start-Service ssh-agent
     Start-Service sshd
+
+    Write-Host "Finished starting OpenSSH services at" (Get-Date).DateTime
 }
 
 # define main function 
@@ -49,6 +72,9 @@ function InstallOpenSsh()
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started installing OpenSSH at" $startDateTime
+
         Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
         
         InstallOpenSshClientAndServer
@@ -56,15 +82,23 @@ function InstallOpenSsh()
         StartOpenSshServices
 
         Write-Host "Successfully installed OpenSSH." -ForegroundColor Green    
+        
         Get-Service "*ssh*"
         ipconfig
         Get-User
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished installing OpenSSH at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
-        Write-Host "Failed to install OpenSSH.`n" -ForegroundColor Red
+        Write-Host "Failed to install OpenSSH." -ForegroundColor Red
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
-
 }
 
 InstallOpenSsh
