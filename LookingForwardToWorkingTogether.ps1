9adbc452dcb0df277a3fc6137659152ba55c8113
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-03 09:44:04 -0700

    Uploaded friendly script.

---
+++ LookingForwardToWorkingTogether.ps1
@@ -0,0 +1,110 @@
+# looking forward to working together 
+
+# you can run this script with: .\LookingForwardToWorkingTogether.ps1 -buddy < your buddy > on Windows or pwsh LookingForwardToWorkingTogether.ps -buddy < your buddy > on Mac or Linux
+
+[CmdletBinding()]
+param(
+    [string] [Parameter(Mandatory = $False)] $buddy = ""
+)
+
+function CheckOs()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "Operating System:" $hostOs -ForegroundColor Green
+    }
+
+    Write-Host "Finished checking operating system at" (Get-Date).DateTime
+    Write-Host ""
+}
+
+function GetBuddy([string]$buddy)
+{
+    if (($buddy -eq $Null) -or ($buddy -eq ""))
+    {
+        Read-Host -Prompt "Please type your buddy's name and press `"Enter`" key on Windows or `"return`" key on Mac or Linux (Example: James)" 
+
+        return $buddy
+    }
+    else
+    {
+        return $buddy
+    }
+}
+
+function CheckParameters([string]$buddy)
+{
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
+
+    Write-Host "`nParameters:"
+    Write-Host "-----------------------"
+    Write-Host ("buddy: {0}" -F $buddy)
+    Write-Host "-----------------------"
+
+    if (($buddy -eq $Null) -or ($buddy -eq ""))
+    {
+        Write-Host "buddy is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "One or more parameter checks are incorret, exiting script." -ForegroundColor Red
+
+        exit -1
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
+}
+
+function LookingForward()
+{
+    Write-Host "`nLooking Forward to Working Together.`n"
+    CheckOs
+
+    $buddy = GetBuddy $buddy
+    CheckParameters $buddy
+
+    try 
+    {
+        $startDateTime = (Get-Date)
+        Write-Host "Started looking forward to you at" $startDateTime
+        
+        $colors = 'White', 'DarkRed', 'Red', 'Yellow', 'DarkYellow', 'Green', 'DarkGreen', 'DarkBlue', 'Blue', 'DarkCyan', 'Cyan', 'Magenta', 'DarkMagenta', 'Black'
+
+        foreach ($color in $colors)
+        {
+            Write-Host ("Looking forward to working together, {0}!" -F $buddy) -Foregroundcolor $color 
+
+            Start-Sleep -s 1 
+        }
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished looking forward to working together at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
+    }
+    catch
+    {
+        Write-Host("Failed to look forward to working with you, {0}." -F $buddy)-ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
+    }
+}
+
+LookingForward
