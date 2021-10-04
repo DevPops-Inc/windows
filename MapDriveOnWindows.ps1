41666bbcafa69a1a08b9cd7530552802816e95c1
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-04 06:59:40 -0700

    Updated OS check and prompts, added parameter checks, execution time and updated exception handling.

--- MapDriveOnWindows.ps1
+++ MapDriveOnWindows.ps1
@@ -8,11 +8,35 @@ param(
     , [string] [Parameter(Mandatory = $False)] $path = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Start checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetDriveLetter([string]$driveLetter)
 {
-    if (($driveLetter -eq $Null) -or ($driveLetter = ""))
+    if (($driveLetter -eq $Null) -or ($driveLetter -eq ""))
     {
-        $driveLetter = Read-Host -Prompt "Please type the letter would you like to map the drive with (Example: D)"
+        $driveLetter = Read-Host -Prompt "Please type the letter would you like to map the drive with and press `"Enter`" key (Example: D)"
         
         return $driveLetter
     }
@@ -26,7 +50,7 @@ function GetPath([string]$path)
 {
     if (($path -eq $Null) -or ($path -eq ""))
     {
-        $path = Read-Host -Prompt "Please type the path of the drive you wish to map (Example: \\Network\Share)"
+        $path = Read-Host -Prompt "Please type the path of the drive you wish to map and press `"Enter`" key (Example: \\Network\Share)"
 
         return $path
     }
@@ -36,47 +60,79 @@ function GetPath([string]$path)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$driveLetter, [string]$path)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
+    Write-Host "Parameters:"
+    Write-Host "-----------------------------------"
+    Write-Host ("driveLetter: {0}" -F $driveLetter)
+    Write-Host ("path       : {0}" -F $path)
+    Write-Host "-----------------------------------"
+
+    if (($driveLetter -eq $Null) -or ($driveLetter -eq ""))
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "driveLetter is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if (($path -eq $Null) -or ($path -eq ""))
+    {
+        Write-Host "path is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+        Write-Host "One or more parameter checks incorrect, exiting script." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
-        break
+        exit -1
     }
-    Write-Host "Finished checking operating system.`n"
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function MapDrive([string]$driveLetter, [string]$path)
 {
     Write-Host "`nMap drive on Windows.`n"
-    #CheckOsForWindows
+    CheckOsForWindows
 
     $driveLetter = GetDriveLetter $driveLetter
     $path = GetPath $path
+    CheckParameters $driveLetter $path
     
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host ("Started mapping {0} drive at {1}" -F $driveLetter, $startDateTime)
+
         New-PSDrive -Name "$driveLetter" -PSProvider FileSystem -Root "$path" -Persist
 
-        Write-Host ("`nSuccessfully mapped {0} drive with  path: {1}" -F $driveLetter, $path) -ForegroundColor Green
+        Write-Host ("Successfully mapped {0} drive with path: {1}" -F $driveLetter, $path) -ForegroundColor Green
 
-        Write-Host "The drives on this computer are: "
+        Write-Host "The drives on this computer are:"
         Get-PSDrive
+
+        $finishedDateTime = (Get-Date)
+        
+        Write-Host ("Finished mapping {0} drive at {1}" -F $driveLetter, $finishedDateTime)
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
-        Write-Host ("`nFailed to map {0} drive with path: {1}" -F $driveLetter, $path) -ForegroundColor Red
+        Write-Host ("Failed to map {0} drive with path: {1}" -F $driveLetter, $path) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
 
         Write-Host "The drives on this computer are: "
         Get-PSDrive
