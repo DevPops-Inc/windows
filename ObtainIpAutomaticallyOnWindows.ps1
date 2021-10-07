7bda991cbda4bd0af03428efdb6529b856e09989
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-07 08:41:08 -0700

    Updated OS check and prompts, added parameter checks, execution time and updated exception handling.

--- ObtainIpAutomaticallyOnWindows.ps1
+++ ObtainIpAutomaticallyOnWindows.ps1
@@ -7,11 +7,36 @@ param(
     [string] [Parameter(Mandatory = $False)] $ipType = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
+
 function GetIpType([string]$ipType)
 {
     if (($ipType -eq $Null) -or ($ipType -eq ""))
     {
-        $iPType = Read-Host - Prompt "Please type which IP type you like to use (IPv4 or IPv6)?"
+        $iPType = Read-Host -Prompt "Please type which IP type you like to use and press `"Enter`" key (IPv4 or IPv6)?"
 
         return $ipType
     }
@@ -21,25 +46,34 @@ function GetIpType([string]$ipType)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$ipType)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
-    {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
-    }
-    else 
-    {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+    Write-Host "Parameters:"
+    Write-Host "-------------------------"
+    Write-Host ("ipType: {0}" -F $ipType)
+    Write-Host "-------------------------"
 
-        Write-Host "Finished checking operating system.`n"
-        break
+    if (($ipType -eq $Null) -or ($ipType -eq ""))
+    {
+        Write-Host "ipType is not set." -ForegroundColor Red
+        $valid = $False
     }
-    Write-Host "Finished checking operating system.`n"
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
+
+        exit -1
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
 }
 
 function ObtainIpAutomatically([string]$ipType)
@@ -48,9 +82,13 @@ function ObtainIpAutomatically([string]$ipType)
     CheckOsForWindows
 
     $ipType = GetIpType $ipType
+    CheckParameters $ipType
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started optaining IP address automatically at" $startDateTime
+
         $adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
         $interface = $adapter | Get-NetIPInterface -AddressFamily $ipType
         
@@ -70,10 +108,21 @@ function ObtainIpAutomatically([string]$ipType)
 
             Write-Host "Successfully obtained IP address automatically." -ForegroundColor Green
         }
+
+        $finishedDateTime = (Get-Date)
+        
+        Write-Host "Finished obtaining IP address automatically at" $finishedDateTime
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host "Failed to obtain IP address automatically" -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
