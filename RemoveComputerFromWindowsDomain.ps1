49d0bf1bd180c9e2891c58805356285a5366089b
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-11 08:22:44 -0700

    Updated OS check and prompts, added parameter checks, execution time and updated exception handling.

--- RemoveComputerFromWindowsDomain.ps1
+++ RemoveComputerFromWindowsDomain.ps1
@@ -8,12 +8,37 @@ param(
     , [string] [Parameter(Mandatory = $False)] $adAdmin = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Start checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetDomain([string]$domain)
 {
     if (($domain -eq $Null) -or ($domain -eq ""))
     {
         $domain = Read-Host -Prompt "Please type the domain you wish to remove the computer from and press `"Enter`" key (Example: developers)"
 
+        Write-Host ""
         return $domain
     }
     else
@@ -28,6 +53,7 @@ function GetAdAdmin([string]$adAdmin)
     {
         $adAdmin = Read-Host -Prompt "Please type the Active Directory admin account and press `"Enter`" key (Example: ad.admin)"
 
+        Write-Host ""
         return $adAdmin
     }
     else 
@@ -36,25 +62,42 @@ function GetAdAdmin([string]$adAdmin)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$domain, [string]$adAdmin)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
-    {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
-    }
-    else 
-    {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+    Write-Host "Parameters:"
+    Write-Host "---------------------------"
+    Write-Host ("domain : {0}" -F $domain)
+    Write-Host ("adAdmin: {0}" -F $adAdmin)
+    Write-Host "---------------------------"
 
-        Write-Host "Finished checking operating system.`n"
-        break
+    if (($domain -eq $Null) -or ($domain -eq ""))
+    {
+        Write-Host "domain is not set." -ForegroundColor red
+        $valid = $False
     }
-    Write-Host "Finished checking operating system.`n"
+
+    if (($adAdmin -eq $Null) -or ($adAdmin -eq ""))
+    {
+        Write-Host "adAdmin is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor red
+
+        exit -1
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function RemoveComputerFromDomain([string]$domain, [string]$adAdmin)
@@ -64,16 +107,29 @@ function RemoveComputerFromDomain([string]$domain, [string]$adAdmin)
 
     $domain = GetDomain $domain
     $adAdmin = GetAdAdmin $adAdmin
+    CheckParameters $domain $adAdmin
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started removing computer at" $startDateTime
+
         Remove-Computer -UnjoinDomaincredential $domain\$adAdmin -PassThru -Verbose -Restart
 
         Write-Host ("Sucessfully removed computer from {0} domain." -F $domain) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished removing computer at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch
     {
         Write-Host ("Failed to remove computer from {0} domain." -F $domain) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
