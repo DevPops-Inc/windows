b558035c10a605dd60f21b3ac716b4cab871134e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-12 06:41:03 -0700

    Updated OS check and prompts, added parameter checks, execution time and updated exception handling.

--- RemoveDistroGroupFromExchange.ps1
+++ RemoveDistroGroupFromExchange.ps1
@@ -7,12 +7,37 @@ param(
     [string] [Parameter(Mandatory = $False)] $distroGroup = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating system is:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetDistroGroup([string]$distroGroup)
 {
     if (($distroGroup -eq $Null) -or ($distroGroup -eq ""))
     {
-        $distroGroup = Read-Host -Prompt "Please type the distribution group would you like to remove from Exchange (Example: devs@devs.com)"
+        $distroGroup = Read-Host -Prompt "Please type the distribution group would you like to remove from Exchange and press `"Enter`" key (Example: devs@devs.com)"
 
+        Write-Host ""
         return $distroGroup
     }
     else
@@ -21,25 +46,35 @@ function GetDistroGroup([string]$distroGroup)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$distroGroup)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
+    Write-Host "Parameters:"
+    Write-Host "-----------------------------------"
+    Write-Host ("distroGroup: {0}" -F $distroGroup)
+    Write-Host "-----------------------------------"
+
+    if (($distroGroup -eq $Null) -or ($distroGroup -eq ""))
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "distroGroup is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
-        break
+        exit -1
     }
-    Write-Host "Finished checking operating system.`n"
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function RemoveDistroGroup([string]$distroGroup)
@@ -48,16 +83,29 @@ function RemoveDistroGroup([string]$distroGroup)
     CheckOsForWindows
 
     $distroGroup = GetDistroGroup $distroGroup
+    CheckParameters $distroGroup
 
     try
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started removing distribution group at" $startDateTime
+
         Remove-DistributionGroup -Identity "$distroGroup"
         
         Write-Host ("Successfully removed {0} from Exchange." -F $distroGroup) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished removing distribution group at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host ("Failed to remove {0} from Exchange." -F $distroGroup) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
