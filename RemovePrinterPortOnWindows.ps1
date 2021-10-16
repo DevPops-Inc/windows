132e621a813a0d5b6d4bfd27010e7b653ef2c904
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-16 15:10:03 -0700

    Updated OS check and prompts, added parameter checks, exeuction time and updated exception handling.

--- RemovePrinterPortOnWindows.ps1
+++ RemovePrinterPortOnWindows.ps1
@@ -7,12 +7,37 @@ param(
     [string] [Parameter(Mandatory = $False)] $ipAddress = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Your operating system is:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetIpAddress([string]$ipAddress)
 {
     if (($ipAddress -eq $Null) -or ($ipAddress -eq ""))
     {
-        $ipAddress = Read-Host -Prompt "Please type the IP address of the printer port you wish to remove and press `"Enter`" key (Example: 127:20:1)"
+        $ipAddress = Read-Host -Prompt "Please type the IP address of the printer port you wish to remove and press `"Enter`" key (Example: 127.20.1)"
 
+        Write-Host ""
         return $ipAddress
     }
     else
@@ -21,25 +46,35 @@ function GetIpAddress([string]$ipAddress)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$ipAddress)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
+    Write-Host "Parameters:"
+    Write-Host "-------------------------------"
+    Write-Host ("ipAddress: {0}" -F $ipAddress)
+    Write-Host "-------------------------------"
+
+    if (($ipAddress -eq $Null) -or ($ipAddress -eq ""))
     {
-        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+        Write-Host "ipAddress is not set." -ForegroundColor red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
     }
     else 
     {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
-        break
+        exit -1
     }
-    Write-Host "Finished checking operating system.`n"
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function RemovePrinterPort([string]$ipAddress)
@@ -48,16 +83,26 @@ function RemovePrinterPort([string]$ipAddress)
     CheckOsForWindows
 
     $ipAddress = GetIpAddress $ipAddress
-    
+    CheckParameters $ipAddress
+
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started removing printer port at" $startDateTime
+        
         Remove-PrinterPort -Name $ipAddress -PrinterHostAddress $ipAddress
         
         Write-Host ("Successfully removed printer port with this IP address: {0}" -F $ipAddress) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished removing printer port at" $finishedDateTime
     }
     catch 
     {
         Write-Host ("Failed to remove printer port with this IP address: {0}" -F $ipAddress) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
