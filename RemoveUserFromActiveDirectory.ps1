3c4f9b45deb681c8ad4dfbb217149676f6764341
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-17 07:47:37 -0700

    Updated parameters, OS check, prompts, added parqmeter checks, execution time, and updated exception handling.

--- RemoveUserFromActiveDirectory.ps1
+++ RemoveUserFromActiveDirectory.ps1
@@ -4,8 +4,32 @@
 
 [CmdletBinding()]
 param(
-    [string] [Parameters(Mandatory = $False)] $username = ""
+    [string] [Parameter(Mandatory = $False)] $username = ""
 )
+
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Your operating system is:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
 
 function GetUsername([string]$username)
 {
@@ -13,6 +37,7 @@ function GetUsername([string]$username)
     {
         $username = Read-Host -Prompt "Please type the user would you like to remove from Active Directory and press `"Enter`" key (Example: software.developer)"
 
+        Write-Host ""
         return $username
     }
     else
@@ -21,43 +46,65 @@ function GetUsername([string]$username)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$username)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
-    {
-        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
-    }
-    else 
-    {
-        Write-Host "Your operating system is:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+    Write-Host "Parameters:"
+    Write-Host "-----------------------------"
+    Write-Host ("username: {0}" -F $username)
+    Write-Host "-----------------------------"
 
-        Write-Host "Finished checking operating system.`n"
-        break
+    if (($username -eq $Null) -or ($username -eq ""))
+    {
+        Write-Host "username is not set." -ForegroundColor Red
+        $valid = $False
     }
-    Write-Host "Finished checking operating system.`n"
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
+
+        exit -1
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
-
 function RemoveUserFromAd([string]$username)
 {
     Write-Host "`nRemove user from Active Directory.`n"
     CheckOsForWindows
 
     $username = GetUsername $username
+    CheckParameters $username
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started removing user at" $startDateTime
+
         Remove-ADGroupMember -Identity $username -force
 
         Write-Host ("Successfully removed {0} from Active Directory." -F $username) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished removing user at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host ("Failed to remove {0} from Active Directory" -F $username) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
