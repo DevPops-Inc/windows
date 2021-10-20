9013ba774397eb9277ec9a769d5610a2e4f435b9
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-20 06:58:42 -0700

    Updated OS check and prompts, added parameter checks, execution time and updated exception handling.

--- RenameComputerOnWindows.ps1
+++ RenameComputerOnWindows.ps1
@@ -7,28 +7,17 @@ param(
     [string] [Parameter(Mandatory = $False)] $newName = ""
 )
 
-function GetNewName([string]$newName)
-{
-    if (($newName -eq $Null) -or ($newName -eq ""))
-    {
-        $newName = Read-Host -Prompt "Please type what you wish the new compter to be and press `"Enter`" key (Example: DEV-PC)"
-
-        return $newName
-    }
-    else 
-    {
-        return $newName
-    }
-}
-
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "`nStarted checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
         Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
@@ -39,7 +28,52 @@ function CheckOsForWindows()
         Write-Host "Finished checking operating system.`n"
         break
     }
-    Write-Host "Finished checking operating system.`n"
+}
+
+function GetNewName([string]$newName)
+{
+    if (($newName -eq $Null) -or ($newName -eq ""))
+    {
+        $newName = Read-Host -Prompt "Please type what you wish the new compter to be and press `"Enter`" key (Example: DEV-PC)"
+
+        Write-Host ""
+        return $newName
+    }
+    else 
+    {
+        return $newName
+    }
+}
+
+function CheckParameters([string]$newName)
+{
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
+
+    Write-Host "Parameters:"
+    Write-Host "---------------------------"
+    Write-Host ("newName: {0}" -F $newName)
+    Write-Host "---------------------------"
+
+    if (($newName -eq $Null) -or ($newName -eq ""))
+    {
+        Write-Host "newName is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
+
+        exit -1
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function RenameComputer([string]$newName)
@@ -48,16 +82,26 @@ function RenameComputer([string]$newName)
     CheckOsForWindows
 
     $newName = GetNewName $newName
+    CheckParameters $newName
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started renaming computer at" $startDateTime
+
         Rename-Computer -NewName "$newName" -DomainCredential Domain01\Admin01 -Restart
 
         Write-Host ("Successfully renamed computer to {0}." -F $newName) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished renaming computer at" $finishedDateTime
     }
     catch
     {
         Write-Host ("Failed to rename the computer to {0}." -F $newName) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
     finally
     {
