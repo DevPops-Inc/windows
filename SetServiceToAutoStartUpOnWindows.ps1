7b78876f739ed1a132a6552eaed31b1de623007e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-23 18:19:02 -0700

    Updated OS check, prompts, and added parameter checks, execution time and improved exception handling.

--- SetServiceToAutoStartUpOnWindows.ps1
+++ SetServiceToAutoStartUpOnWindows.ps1
@@ -7,12 +7,37 @@ param(
     [string] [Parameter(Mandatory = $False)] $serviceName = ""
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host "'"
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
 function GetServiceName([string]$serviceName)
 {
     if (($serviceName -eq $Null) -or ($serviceName -eq ""))
     {
         $serviceName = Read-Host -Prompt "Please type the service you wish to set to automatic start up and press `"Enter`" key (Example: XboxNetApiSvc)"
 
+        Write-Host ""
         return $serviceName
     }
     else
@@ -21,25 +46,35 @@ function GetServiceName([string]$serviceName)
     }
 }
 
-function CheckOsForWindows()
+function CheckParameters([string]$serviceName)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "Start checking parameters at" (Get-Date).DateTime
+    $valid = $True
 
-    if ($hostOs -eq "Win32NT")
+    Write-Host "Parameters:"
+    Write-Host "-----------------------------------"
+    Write-Host ("serviceName: {0}" -F $serviceName)
+    Write-Host "-----------------------------------"
+
+    if (($serviceName -eq $Null) -or ($serviceName -or ""))
     {
-        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+        Write-Host "serviceName is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
     }
     else 
     {
-        Write-Host "Operating System:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
-        break
+        exit -1
     }
-    Write-Host "Finished checking operating system.`n"
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function SetServiceToAutoStart([string]$serviceName)
@@ -51,18 +86,34 @@ function SetServiceToAutoStart([string]$serviceName)
     Get-Service
 
     $serviceName = GetServiceName $serviceName
+    CheckParameters $serviceName
 
     try 
     {
+        $startDateTime = (Get-Date)
+        
+        Write-Host "Started setting service to automatic startup at" $startDateTime
+
         Set-Service $serviceName -StartupType Automatic
 
         Write-Host ("Successfully set {0} to automatically startup." -F $serviceName) -ForegroundColor Green
 
         Get-Service -Name $serviceName
+
+        $finishedDateTime = (Get-Date)
+        
+        Write-Host "Finished setting service to automatic startup at" $finishedDateTime
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch
     {
         Write-Host ("Failed to set {0} to automatically startup." -F $serviceName) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
