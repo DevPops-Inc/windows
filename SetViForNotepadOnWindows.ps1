15143989f31f7c2395b1dcf4125b667e38d818e0
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-24 08:15:36 -0700

    Updated parameters, OS check and exception handling then added get functions and execution time.

--- SetViForNotepadOnWindows.ps1
+++ SetViForNotepadOnWindows.ps1
@@ -1,23 +1,53 @@
-# set vi for Notepad in PowerShell
+# set vi for Notepad on Windows
 
 # you have to update the $Profile for this change to persist
 
 [CmdletBinding()]
 param(
-      [string] [Parameter(Mandatory = $False)] $newAlias = "vi"
-    , [string] [Parameter(Mandatory = $False)] $app = "notebook"
+    [string] [Parameter(Mandatory = $False)] $newAlias = "vi", 
+    [string] [Parameter(Mandatory = $False)] $app = "notebook"
 )
+
+function GetNewAlias([string]$newAlias)
+{
+    if (($newAlias -eq $Null) -or ($newAlias -eq ""))
+    {
+        Read-Host -Prompt "Please type the new alias and press `"Enter`" key (Example: vi)"
+
+        Write-Host ""
+        return $newAlias
+    }
+    else 
+    {
+        return $newAlias
+    }
+}
+
+function GetApp([string]$app)
+{
+    if (($app -eq $Null) -or ($app -eq ""))
+    {
+        Read-Host -Prompt "Please type the application you wish to create an alias for and press `"Enter`" key (Example: notebook)"
+
+        Write-Host ""
+        return $app
+    }
+    else 
+    {
+        return $app
+    }
+}
 
 function CheckParameters([string]$newAlias, [string]$app)
 {
-    Write-Host "`nStarted checking parameters..."
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
     $valid = $True
 
-    Write-Host "`nParameters:"
-    Write-Host "----------------------------------------"
+    Write-Host "Parameters:"
+    Write-Host "---------------------------------"
     Write-Host ("newAlias    : {0}" -F $newAlias)
     Write-Host ("app        : {0}" -F $app)
-    Write-Host "----------------------------------------`n"
+    Write-Host "----------------------------------"
     
     if (($newAlias -eq $Null) -or ($newAlias -eq ""))
     {
@@ -31,8 +61,6 @@ function CheckParameters([string]$newAlias, [string]$app)
         $valid = $False
     }
 
-    Write-Host "Finished checking parameters."
-
     if ($valid -eq $True)
     {
         Write-Host "All parameters checks passed." -ForegroundColor Green
@@ -40,19 +68,25 @@ function CheckParameters([string]$newAlias, [string]$app)
     else
     {
         Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
-        exit $result
+
+        exit -1
     }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
     Write-Host ""
 }
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
         Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host "'"
     }
     else 
     {
@@ -60,28 +94,40 @@ function CheckOsForWindows()
         
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
 }
 
 function SetViForNotepad([string]$newAlias, [string]$app)
 {
-    Write-Host "`nSet vi for Notepad in PowerShell`n"
-    CheckParameters $newAlias $app
+    Write-Host "`nSet vi for Notepad on Windows`n"
     CheckOsForWindows
+
+    $newAlias = GetNewAlias $newAlias
+    $app = GetApp $app
+    CheckParameters $newAlias $app
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started setting vi for Notepad at" $startDateTime
+
         New-Alias vi notepad
 
         Write-Host "Successfully set vi for Notepad." -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished setting vi for Notepad at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
         Write-Host "Failed to set vi for Notepad." -ForegroundColor Red
-        Write-Host $_ -ForegroundColor
+        Write-Host $_ -ForegroundColor Red
         Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
