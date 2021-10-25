8ab7913af005a1c43c744f9c279b242f0fff1790
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-25 05:15:23 -0700

    Updated OS check and added execution time.

--- ShutdownComputerOnWindows.ps1
+++ ShutdownComputerOnWindows.ps1
@@ -2,12 +2,15 @@
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
         Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
@@ -15,29 +18,37 @@ function CheckOsForWindows()
         
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
 }
 
 function ShutdownComputer()
 {
     Write-Host "`nShutdown computer on Windows.`n"
-    #CheckOsForWindows
+    CheckOsForWindows
 
-    Write-Host "Please save documents and close applications."
-    Pause 
+    Read-Host -Prompt "Please save documents and close applications. Press any key to continue"
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started shutting down computer at" $startDateTime
+
         Stop-Computer -ComputerName localhost
         
         Write-Host "Succesfully shutting down computer now." -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished shutting down computer at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
-        Write-Host "Faild to shutdown computer." -ForegroundColor Red
+        Write-Host "Failed to shutdown computer." -ForegroundColor Red
         Write-Host $_ -ForegroundColor Red
         Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
