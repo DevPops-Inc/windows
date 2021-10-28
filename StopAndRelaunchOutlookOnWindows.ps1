4c6a2c3c4fabc0b0a7beef1769458d21dd07d7ea
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-28 09:09:13 -0700

    Revised scripts for better logging.

--- StopAndRelaunchOutlookOnWindows.ps1
+++ StopAndRelaunchOutlookOnWindows.ps1
@@ -2,22 +2,93 @@
 
 [CmdletBinding()]
 param(
-      [string] [Parameter(Mandatory = $False)] $processName = "OUTLOOK"
-    , [int]    [Parameter(Mandatory = $False)] $seconds = 5
-    , [string] [Parameter(Mandatory = $False)] $applicationName = "outlook.exe"
+    [string] [Parameter(Mandatory = $False)] $processName = "OUTLOOK", 
+    [int]    [Parameter(Mandatory = $False)] $seconds = 5, 
+    [string] [Parameter(Mandatory = $False)] $applicationName = "outlook.exe"
 )
 
-function CheckParameters([string]$processName, [int]$seconds, [string]$applicationName)
+function CheckOsForWindows()
 {
-    Write-Host "`nStarted checking parameters..."
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host "'"
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        
+        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break
+    }
+}
+
+function GetProcessName([string]$processName)
+{
+    if (($processName -eq $Null) -or ($processName -eq ""))
+    {
+        Read-Host -Prompt "Please type the process name and press `"Enter`" key (Example: OUTLOOK)"
+
+        Write-Host ""
+        return $processName
+    }
+    else 
+    {
+        return $processName
+    }
+}
+
+function GetSeconds([int]$seconds)
+{
+    if (($seconds -eq $Null) -or ($seconds -eq ""))
+    {
+        Read-Host -Prompt "Please type the number of seconds and press `"Enter`" key (Example: 5)" 
+
+        Write-Host ""
+        return $seconds
+    }
+    else 
+    {
+        return $seconds
+    }
+}
+
+function GetApplicationName([string]$applicationName)
+{
+    if (($applicationName -eq $Null) -or ($applicationName -eq ""))
+    {
+        Read-Host -Prompt "Please type the application name and press `"Enter`" key (Example: outlook.exe)" 
+
+        Write-Host ""
+        return $applicationName
+    }
+    else 
+    {
+        return $applicationName
+    }
+}
+
+function CheckParameters([string]$processName, 
+                         [int]   $seconds, 
+                         [string]$applicationName)
+{
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
     $valid = $True
 
-    Write-Host "`nParameters:"
-    Write-Host "----------------------------------------"
+    Write-Host "Parameters:"
+    Write-Host "-------------------------------------------"
     Write-Host ("processName    : {0}" -F $processName)
     Write-Host ("seconds        : {0}" -F $seconds)
     Write-Host ("applicationName: {0}" -F $applicationName)
-    Write-Host "----------------------------------------`n"
+    Write-Host "-------------------------------------------"
     
     if (($processName -eq $Null) -or ($processName -eq ""))
     {
@@ -37,8 +108,6 @@ function CheckParameters([string]$processName, [int]$seconds, [string]$applicati
         $valid = $False
     }
 
-    Write-Host "Finished checking parameters."
-
     if ($valid -eq $True)
     {
         Write-Host "All parameters checks passed." -ForegroundColor Green
@@ -46,45 +115,46 @@ function CheckParameters([string]$processName, [int]$seconds, [string]$applicati
     else
     {
         Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
-        exit $result
+        
+        exit -1
     }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
     Write-Host ""
 }
 
-function CheckOsForWindows()
+
+
+function StopAndRelaunchOutlook([string]$processName, 
+                                [int]   $seconds, 
+                                [string]$applicationName)
 {
-    Write-Host "`nChecking operating system..."
-    $hostOs = [System.Environment]::OSVersion.Platform
+    Write-Host "`nStop and relaunch Outlook on Windows.`n"
+    CheckOsForWindows
 
-    if ($hostOs -eq "Win32NT")
-    {
-        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
-    }
-    else 
-    {
-        Write-Host "Operating System:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system.`n"
-        break
-    }
-    Write-Host "Finished checking operating system.`n"
-}
-
-function StopAndRelaunchOutlook([string]$processName, [int]$seconds, [string]$applicationName)
-{
-    Write-Host "`nStop and relaunch Outlook function on Windows.`n"
+    $processName = GetProcessName $processName
+    $seconds = GetSeconds $seconds
+    $applicationName = GetApplicationName $applicationName
     CheckParameters $processName $seconds $applicationName
-    CheckOsForWindows 
-
+     
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started stopping and relaunching Outlook at" $startDateTime
+
         Stop-Process -ProcessName $processName
         Start-Sleep -Seconds $seconds
         Start-Process -FilePath $applicationName 
 
         Write-Host "Successfully stopped and restarted Outlook." -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)
+        
+        Write-Host "Finished stopping and relaunching Outlook at" $finishedDateTime
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
--- StopProcessOnWindows.ps1
+++ StopProcessOnWindows.ps1
@@ -7,25 +7,17 @@ param(
     [string] [Parameter(Mandatory = $False)] $processName = ""
 )
 
-function GetProcessName([string]$processName)
-{
-    $processName = Read-Host -Prompt "Please type the process you would like to stop and press `"Enter`" key (Example: outlook.exe)"
-
-    return $processName
-}
-else 
-{
-    return $processName
-}
-
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
         Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
@@ -33,10 +25,53 @@ function CheckOsForWindows()
         
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
+}
+
+function GetProcessName([string]$processName)
+{
+    $processName = Read-Host -Prompt "Please type the process you would like to stop and press `"Enter`" key (Example: outlook.exe)"
+
+    Write-Host ""
+    return $processName
+}
+else 
+{
+    return $processName
+}
+
+function CheckParameters([string]$processName)
+{
+    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    $valid = $True
+
+    Write-Host "Parameters:"
+    Write-Host "-----------------------------------"
+    Write-Host ("processName: {0}" -F $processName)
+    Write-Host "-----------------------------------"
+
+    if (($processName -eq $Null) -or ($processName -eq ""))
+    {
+        Write-Host "processName is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed." -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
+
+        break
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function StopProcess([string]$processName)
@@ -46,16 +81,27 @@ function StopProcess([string]$processName)
 
     Write-Host "The processes running on this computer are:"
     Get-Process
+    Write-Host ""
 
     $processName = GetProcessName $processName
+    CheckParameters $processName
 
     try 
     {
+        $startDateTime = (Get-Date)
+        Write-Host "Started stopping process at" $startDateTime
+
         Stop-Process -processname $processName
 
         Write-Host ("Successfully stopped {0} process." -F $processName) -ForegroundColor Green
 
         Get-Process -Name $processName
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished stopping process at" $finishedDateTime
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch 
     {
