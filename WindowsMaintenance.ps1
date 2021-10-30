53c017ed848cee5db5e0f8ab126f9adc17c189c7
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-10-30 09:04:35 -0700

    Updated OS check and created functions for each task.

--- WindowsMaintenance.ps1
+++ WindowsMaintenance.ps1
@@ -2,52 +2,84 @@
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
         Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
         Write-Host "Operating System:" $hostOs
         
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
+        Write-Host "Sorry but this script only runs on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
         break
     }
-    Write-Host "Finished checking operating system.`n"
+}
+
+function CheckDisk()
+{
+    Write-Host "Started checking disk at" (Get-Date).DateTime
+
+    Write-Output y | chkdsk /f/r c:
+
+    Write-Host "Finished checking disk at" (Get-Date).DateTime
+    Write-Host ""
+}
+
+function SystemsFileCheck()
+{
+    Write-Host "Started systems file check at" (Get-Date).DateTime
+
+    SFC /scannow
+
+    Write-Host "Finished systems file check at" (Get-Date).DateTime
+    Write-Host ""
+}
+
+function ScanWindowsImage()
+{
+    Write-Host "Started scanning Windows image at" (Get-Date).DateTime
+
+    Dism /Online /Cleanup-Image /ScanHealth
+
+    Write-Host "Finished scanning Windows image at" (Get-Date).DateTime
+    Write-Host ""
+}
+
+function DefragDisk()
+{
+    Write-Host "Started defragging disk at" (Get-Date).DateTime
+
+    defrag c: /u
+
+    Write-Host "Finished defragging disk at" (Get-Date).DateTime
+    Write-Host ""
 }
 
 function WindowsMaintenance()
 {
     Write-Host "`nWindows maintenance.`nYou need to elevate permissions before running this script: Start-Process Powershell -Verb runAs`n"
 
-    #CheckOsForWindows
-
-    $startDateTime = (Get-Date)
-    Write-Host "Starting Windows Maintenance at:" $startDateTime
+    CheckOsForWindows
 
     try 
     {
-        Write-Host "`nStarting check disk..."
-        Write-Output y | chkdsk /f/r c:
-        Write-Host "Finished checking disk."
-    
-        Write-Host "`nStarting systems file check..."
-        SFC /scannow
-        Write-Host "Finished systems file check."
-    
-        Write-Host "`nScanning Windows image..."
-        Dism /Online /Cleanup-Image /ScanHealth
-        Write-Host "Finished scanning Windows image."
-        
-        Write-Host "`nStarting to defrag disk..."
-        defrag c: /u
-        Write-Host "Finished defragging disk."
+        $startDateTime = (Get-Date)
+        Write-Host "Started Windows maintenance at:" $startDateTime
 
+        CheckDisk
+        SystemsFileCheck
+        ScanWindowsImage
+        DefragDisk
+        
         Write-Host "`nSuccessfully performed maintenance on Windows." -ForegroundColor Green
     }
     catch
@@ -60,12 +92,14 @@ function WindowsMaintenance()
     finally
     {
         $finishedDateTime = (Get-Date)
-        Write-Host "`nFinished Windows maintenance at:" $finishedDateTime
+        Write-Host "Finished Windows maintenance at:" $finishedDateTime
+        
         $duration = New-TimeSpan $startDateTime $finishedDateTime
+        
         Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
 
-        Write-Host "`nPlease save your documents and close applications."
-        Pause
+        Read-Host -Prompt "Please save your documents and close applications.`nPress any key to restart your computer"
+        
         shutdown /r /t 0
     }
 }
