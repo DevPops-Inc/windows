992e856b32e1bb18bae70cb751e0813dc7864399
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-01-28 06:41:47 -0800

    Cleaned up script so it prints in terminal better.

--- CreateStandardUserOnWindows.ps1
+++ CreateStandardUserOnWindows.ps1
@@ -4,31 +4,32 @@
 
 [CmdletBinding()]
 param(
-      [string]       [Parameter(Mandatory = $False)] $standardUser = ""
-    , [securestring] [Parameter(Mandatory = $False)] $password = $Null
-    , [string]       [Parameter(Mandatory = $False)] $description = ""
+  [string]       [Parameter(Mandatory = $False)] $standardUser = "", # you can set the username here 
+  [securestring] [Parameter(Mandatory = $False)] $password = $Null, # you can set the password here 
+  [string]       [Parameter(Mandatory = $False)] $description = "" # you can 
 )
 
 function CheckOsForWindows()
 {
-  Write-Host "`nCheck operation system at" (Get-Date).DateTime
+  Write-Host "Started checking operation system at" (Get-Date).DateTime
   $hostOs = [System.Environment]::OSVersion.Platform
 
   if ($hostOs -eq "Win32NT")
   {
-    Write-Host "Operating System: " (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+    Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
 
     Write-Host "Finished checking operating system at" (Get-Date).DateTime
     Write-Host ""
   }
   else 
   {
-    Write-Host "Operating System: " $hostOs
+    Write-Host "Operating System:" $hostOs 
 
     Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
 
     Write-Host "Finished checking operating system at" (Get-Date).DateTime
     Write-Host ""
+
     break
   }
 }
@@ -37,8 +38,9 @@ function GetStandardUser([string]$standardUser)
 {
   if (($standardUser -eq $Null) -or ($standardUser -eq ""))
     {
-      $standardUser = Read-Host -Prompt "`nPlease type the standard user name and press `"Enter`" key (Example: Standard.User)"
+      $standardUser = Read-Host -Prompt "Please type the standard user name and press `"Enter`" key (Example: Standard.User)"
 
+      Write-Host ""
       return $standardUser
     }
     else 
@@ -51,8 +53,9 @@ function GetPassword([securestring]$password)
 {
   if (($password -eq $Null) -or ($password -eq ""))
   {
-    $password = Read-Host -Prompt "`nPlease type the password and press `"Enter`" key (Example: Password1234)" -AsSecureString
+    $password = Read-Host -Prompt "Please type the password and press `"Enter`" key (Example: Password1234)" -AsSecureString
 
+    Write-Host ""
     return $password
   }
   else 
@@ -65,8 +68,9 @@ function GetDescription([string]$description)
 {
   if (($description -eq $Null) -or ($description -eq ""))
   {
-    $description = Read-Host -Prompt "`nPlease type the description and press `"Enter`" key (Example: Standard User)"
+    $description = Read-Host -Prompt "Please type the description and press `"Enter`" key (Example: Standard User)"
 
+    Write-Host ""
     return $description
   }
   else
@@ -79,15 +83,15 @@ function CheckParameters([string]      $standardUser,
                          [securestring]$password, 
                          [string]      $description)
 {
-  Write-Host "`nStarted checking parameters at" (Get-Date).DateTime
+  Write-Host "Started checking parameters at" (Get-Date).DateTime
   $valid = $True
 
-  Write-Host "`nParameters:"
-  Write-Host "----------------------------------------"
+  Write-Host "Parameters:"
+  Write-Host "-------------------------------------"
   Write-Host ("standardUser: {0}" -F $standardUser)
   Write-Host ("password    : {0}" -F "***")
   Write-Host ("description : {0}" -F $description)
-  Write-Host "----------------------------------------"
+  Write-Host "-------------------------------------"
 
   if (($standardUser -eq $Null) -or ($standardUser -eq ""))
   {
@@ -97,7 +101,7 @@ function CheckParameters([string]      $standardUser,
 
   if (($password -eq $Null) -or ($password -eq ""))
   {
-    Write-Host "password is not set." -ForegroundColor
+    Write-Host "password is not set." -ForegroundColor Red
     $valid = $False
   }
 
@@ -107,18 +111,18 @@ function CheckParameters([string]      $standardUser,
     $valid = $False
   }
 
-  Write-Host "Finished checking parameters at" (Get-Date).DateTime
-
   if ($valid -eq $True)
   {
     Write-Host "All parameter checks passed.`n"  -ForegroundColor Green
   }
   else 
   {
-    Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
-
-    exit -1
+    Write-Host "One or more parameters are incorrect." -ForegroundColor Red
+    break
   }
+
+  Write-Host "Finished checking parameters at" (Get-Date).DateTime
+  Write-Host ""
 }
 
 function CreateStandardUser([string]$standardUser, [securestring]$password, [string]$description)
@@ -143,23 +147,27 @@ function CreateStandardUser([string]$standardUser, [securestring]$password, [str
     Set-LocalUser -Name $standardUser -PasswordNeverExpires 1
 
     # check if new user has been added
-    Write-Host ("`nSuccessfully created standard user: {0}`n" -F $standardUser) -ForegroundColor Green
+    Write-Host ("Successfully created standard user: {0}" -F $standardUser) -ForegroundColor Green
 
-    Write-Host "The users on the this computer are: `n"
+    Write-Host "The users on the this computer are:"
     Get-LocalUser
 
     $finishedDateTime = (Get-Date)
     Write-Host "Finished creating standard user at" $finishedDateTime
+    
     $duration = New-TimeSpan $startDateTime $finishedDateTime
 
     Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
+
+    Write-Host ""
   }
   catch 
   {
-    Write-Host ("`nFailed to create standard user {0}.`n" -F $standardUser) -ForegroundColor Red
+    Write-Host ("Failed to create standard user {0}." -F $standardUser) -ForegroundColor Red
 
     Write-Host $_ -ForegroundColor Red
     Write-Host $_.ScriptStackTrace -ForegroundColor Red
+    Write-Host ""
   }
 }
 
