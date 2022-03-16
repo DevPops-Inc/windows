ff97706de72222d5d5bcad2acdcafd04274832aa
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-03-15 18:23:24 -0700

    Cleaned up spacing.

--- CheckDiskOnWindows.ps1
+++ CheckDiskOnWindows.ps1
@@ -15,7 +15,6 @@ function CheckOsForWindows()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        
         Write-Host "Sorry but this script only runs on Windows." -ForegroundColor Red
 
         Write-Host "Finished checking operating system at" (Get-Date).DateTime
