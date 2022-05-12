4fe095b198519f0e2420bcfab25cc684a5ed8847
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-05-11 18:23:07 -0700

    Revised wording in negative case.

--- checkOsForWindowsInPython.py
+++ checkOsForWindowsInPython.py
@@ -19,7 +19,7 @@ def checkOsForWindows():
         
         print("")
     else:
-        print(Fore.RED + "Sorry this script only works on Windows." + Style.RESET_ALL)
+        print(Fore.RED + "Sorry this script only runs on Windows." + Style.RESET_ALL)
     
         print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
         
