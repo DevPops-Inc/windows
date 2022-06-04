27deb2abb6520b5bb09c72ce5e5ba98285c9a7a4
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-06-04 07:25:45 -0700

    Uploaded Windows maintenance Python scripts.

---
+++ windowsMaintenance.py
@@ -0,0 +1,69 @@
+#!/bin/python
+
+# Windows maintenance
+
+# run this script as admin
+
+import colorama, os, sys, traceback
+from colorama import Fore, Style 
+from datetime import datetime
+colorama.init()
+
+def checkOsForWindows():
+    print("Started checking operating system at ", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('ver'))
+        
+        print(Style.RESET_ALL + "Finished checking operating system at ", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        print("")
+    else: 
+        print(Fore.RED + "Sorry but this script only runs on Windows." + Style.RESET_ALL)
+
+        print("Finished checking operating system at ", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        exit("")
+    
+
+def runWindowsMaintenance():
+    print("\nRun Windows maintenance.\n")
+    checkOsForWindows()
+
+    try: 
+        startDateTime = datetime.now()
+        
+        print("Started running Windows maintenance at ", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        maintenance = ['echo y | chkdsk /f/r c:', 'SFC /scannow', 'Dism /Online /Cleanup-Image /ScanHealth']
+
+        for job in maintenance: 
+            os.system(job)
+
+        os.system('PowerShell "Get-PhysicalDisk | Format-Table -AutoSize"')
+        print("Do you want to defrag the HDD (not recommended for SSD drives)?")
+        answer = str(input("Please press \"Y\" or \"N\" and press \"Enter\" key: "))
+        if answer == "Y" or answer == "y":
+            os.system('defrag c: /u')
+            
+        print(Fore.GREEN + "Successfully ran maintenance on Windows." + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+
+        print("Finished running Windows maintenance at ", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        duration = finishedDateTime - startDateTime 
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+
+        print("Please save your work and close applications.")
+        str(input("Press any key to continue."))
+        os.system('shutdown /r /t 0')
+    except Exception as e: 
+        print(Fore.RED + "Failed to run maintenance on Windows.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+runWindowsMaintenance()
---
+++ windowsOrMacMaintenance.py
@@ -0,0 +1,122 @@
+#!/bin/python
+
+# Windows or Mac maintenance
+
+# run this script as admin on Windows
+
+import colorama, os, sys, traceback
+from colorama import Fore, Style 
+from datetime import datetime
+colorama.init()
+
+def checkOs():
+    print("Started checking operating system at ", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('ver'))
+        print(Style.RESET_ALL)
+        operatingSystem = "Windows"
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('sw_vers'))
+        print(Style.RESET_ALL)
+        operatingSystem = "macOS"
+    else: 
+        print(Fore.RED + "Sorry but this script only runs on Windows or macOS." + Style.RESET_ALL)
+
+        print("Finished checking operating system at ", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        exit("")
+    print("Finished checking operating system at ", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    print("")
+    return operatingSystem
+    
+def runWindowsMaintenance():
+    startDateTime = datetime.now()
+    
+    print("Started running Windows maintenance at ", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+    maintenance = ['echo y | chkdsk /f/r c:', 'SFC /scannow', 'Dism /Online /Cleanup-Image /ScanHealth']
+
+    for job in maintenance: 
+        os.system(job)
+
+    os.system('PowerShell "Get-PhysicalDisk | Format-Table -AutoSize"')
+    print("Do you want to defrag the HDD (not recommended for SSD drives)?")
+    answer = str(input("Please press \"Y\" or \"N\" and press \"Enter\" key: "))
+    if answer == "Y" or answer == "y":
+        os.system('defrag c: /u')
+
+    print(Fore.GREEN + "Successfully ran maintenance on Windows." + Style.RESET_ALL)
+
+    finishedDateTime = datetime.now()
+
+    print("Finished running Windows maintenance at ", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+    duration = finishedDateTime - startDateTime 
+    print("Total execution time: {0} second(s)".format(duration.seconds))
+    print("")
+    
+    print("Please save your work and close applications.")
+    str(input("Press any key to continue."))
+    os.system('shutdown /r /t 0')
+
+def runMacMaintenance():
+    startDateTime = datetime.now()
+    
+    print("Started running Mac maintenance at ", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+    maintenance = ['sudo mdutil -i on /', 'softwareupdate --install --all']
+
+    for jobs in maintenance: 
+        os.system(jobs)
+
+    os.system('diskutil list')
+
+    answer = str(input("Please type 1 if your hard drive is \"Macintosh HD\" or 2 if it's \"MacOS\" and press \"return\" key: "))
+    
+    if answer == "1": 
+        os.system('diskutil verifyVolume "Macintosh HD"')
+        os.system('diskutil repairVolume "Macintosh HD"')
+    elif answer == "2": 
+        os.system('diskutil verifyVolume MacOS')
+        os.system('diskutil repairVolume MacOS')
+
+    print(Fore.GREEN + "Successfully ran Mac maintenance." + Style.RESET_ALL)
+
+    finishedDateTime = datetime.now()
+
+    print("Finished running Mac maintenance at ", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+    duration = finishedDateTime - startDateTime
+    print("Total execution time: {0} second(s)".format(duration.seconds))
+    print("")
+
+    print("Please save your documents and close applications.")
+    str(input("Press any key to restart Mac."))
+    os.system('reboot')
+
+def computerMaintenance():
+    print("\nRun computer maintenance.\n")
+    operatingSystem = checkOs()
+
+    if operatingSystem == "Windows": 
+        try: 
+            runWindowsMaintenance()
+        except Exception as e: 
+            print(Fore.RED + "Failed to run Windows maintenance.")
+            print(e)
+            print(traceback.print_stack)
+            exit("" + Style.RESET_ALL)
+    elif operatingSystem == "macOS":
+        try: 
+            runMacMaintenance()
+        except Exception as e: 
+            print(Fore.RED + "Failed to run Mac maintenance.")
+            print(e)
+            print(traceback.print_stack)
+            exit("" + Style.RESET_ALL)
+
+computerMaintenance()
