06d0f5453565492e51477a5b9667277be2362599
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-09-06 06:36:10 -0700

    Upload get Wi-Fi profiles script.

---
+++ getWiFiProfilesOnWindows.py
@@ -0,0 +1,56 @@
+#!/bin/python
+
+# get Wi-Fi profiles on Windows 
+
+import colorama, os, sys, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
+
+
+def checkOsForWindows(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System: ")
+        os.system('ver')
+        print(Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        print("")
+    
+    else: 
+        print(Fore.RED + "Sorry but this script only runs on Windows." + Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        exit("")
+
+
+def getWiFiProfiles(): 
+    print("\nGet Wi-Fi profiles on Windows.\n")
+    checkOsForWindows()
+
+    try: 
+        startDateTime = datetime.now()
+        
+        print("Started getting Wi-Fi profiles at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        os.system('netsh wlan show profiles')
+        print(Fore.GREEN + "Successfully got Wi-Fi profiles." + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+        
+        print("Finished getting Wi-Fi profiles at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        print("")
+
+    except Exception as e: 
+        print(Fore.RED + "Failed to get Wi-Fi profiles.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+
+getWiFiProfiles()
