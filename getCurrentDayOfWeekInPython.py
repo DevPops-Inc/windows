da8dc79c662e8dd020deb69717ca9fa253c80dd8
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-11-06 06:46:38 -0800

    Upload OS-agnostic get current day script.

---
+++ getCurrentDayOfWeekInPython.py
@@ -0,0 +1,64 @@
+#!/bin/python
+
+# get current day of week in Python
+
+import colorama, os, sys, time, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
+
+
+def checkOs(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System: ", end="")
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Windows"
+
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System: ")
+        os.system('sw_vers')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "macOS"
+
+    elif sys.platform == "linux": 
+        print(Fore.GREEN + "Operating System: ")
+        os.system('sw_vers')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Linux"
+
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    print("")
+    return operatingSystem
+
+
+def getCurrentDayOfWeek(): 
+    print("\nGet current day of the week in Python.\n")
+    checkOs()
+
+    try: 
+        startDateTime = datetime.now()
+        
+        print("Started getting current day of the week at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        print(Fore.BLUE + "The current day of the week is: {0}.".format(time.strftime('%A')))
+        print(Fore.GREEN + "Successfully got the current day of the week." + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+
+        print("Finished getting current day of the week at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+        
+    except Exception as e: 
+        print(Fore.RED + "Failed to get current day of the week.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+
+getCurrentDayOfWeek()
