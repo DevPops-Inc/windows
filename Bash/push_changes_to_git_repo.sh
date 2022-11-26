c831cc64378ce4dba737c7f278dbbdaee8f930b2
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-11-26 09:40:12 -0800

    Changed directory to lowercase.

--- Bash/push_changes_to_git_repo.sh
+++ Bash/push_changes_to_git_repo.sh
@@ -11,12 +11,12 @@ check_os() {
     echo "Started checking operating system at $(date)"
 
     if [[ $OSTYPE == 'darwin'* ]]; then 
-        tput setaf 2; echo -e "Operating system:\n$(sw_vers)"; tput sgr0
+        tput setaf 2; echo -e "Operating System:\n$(sw_vers)"; tput sgr0
         
         echo "Finished checking operating system at $(date)"
         echo ""
     else 
-        tput setaf 2; echo "Operating system: $OSTYPE"; tput sgr0
+        tput setaf 2; echo "Operating System: $OSTYPE"; tput sgr0
 
         echo "Finished checking oeprating system at $(date)"
         echo ""
