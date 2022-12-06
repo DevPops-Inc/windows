cadc832a080a2d89583cab5449fe2b15d1ed1c98
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-05 20:06:28 -0800

    Fixed parameters.

--- PowerShell/CheckProgramOnWindows.ps1
+++ PowerShell/CheckProgramOnWindows.ps1
@@ -1,5 +1,7 @@
 # check program on Windows
 
+# you can run this script with: .\CheckProgramOnWindows.ps1 -checkProgram "< program >"
+
 [CmdletBinding()]
 param (
     [string] [Parameter(Mandatory = $False)] $checkProgram = ""
@@ -82,10 +84,10 @@ function GetWinPrograms()
 
 function CheckParameters([string]$checkProgram)
 {
-    Write-Host "Started checking parameters at" (Get-Date).DateTime
+    Write-Host "Started checking parameter(s) at" (Get-Date).DateTime
     $valid = $true
 
-    Write-Host "Parameters:"
+    Write-Host "Parameter(s):"
     Write-Host "-------------------------------------"
     Write-Host ("checkProgram: {0}" -F $checkProgram)
     Write-Host "-------------------------------------"
@@ -100,14 +102,14 @@ function CheckParameters([string]$checkProgram)
     {
         Write-Host "All parameter check(s) passed." -ForegroundColor Green
 
-        Write-Host "Finished checking parameters at" (Get-Date).DateTime
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
         Write-Host ""
     }
     else 
     {
         Write-Host "One or more parameters are incorrect." -ForegroundColor Red
         
-        Write-Host "Finished checking parameters at" (Get-Date).DateTime
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
         Write-Host ""
 
         break
@@ -126,6 +128,7 @@ function CheckProgram()
     try
     {
         $startDateTime = (Get-Date)
+        
         Write-Host ("Started checking {0} at {1}" -F $checkProgram, $startDateTime.DateTime)
 
         if ($winProgramsList -contains $checkProgram)
@@ -140,6 +143,7 @@ function CheckProgram()
         Write-Host ("Successfully checked {0}." -F $checkProgram) -ForegroundColor Green
 
         $finishedDateTime = (Get-Date)
+
         Write-Host ("Finished checking {0} at {1}" -F $checkProgram, $finishedDateTime.DateTime)
 
         $duration = New-TimeSpan $startDateTime $finishedDateTime
