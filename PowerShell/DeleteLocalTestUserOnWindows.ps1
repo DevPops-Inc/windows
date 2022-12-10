9bb19c52ef4451722e1cbd49f9beb1c7b38e743e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-09 18:41:38 -0800

    Cleaned spacing.

--- PowerShell/DeleteLocalTestUserOnWindows.ps1
+++ PowerShell/DeleteLocalTestUserOnWindows.ps1
@@ -22,7 +22,6 @@ function CheckOsForWindows()
     else 
     {
         Write-Host "Operating System:" $hostOs
-
         Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
 
         Write-Host "Finished checking operating system at" (Get-Date).DateTime
