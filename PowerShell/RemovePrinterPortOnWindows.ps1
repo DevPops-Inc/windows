92174bb1dff723d2273fc9cb7c57c843584c288a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-22 19:24:32 -0800

    Revised date time prompts in OS checks.

--- PowerShell/RemovePrinterPortOnWindows.ps1
+++ PowerShell/RemovePrinterPortOnWindows.ps1
@@ -65,16 +65,19 @@ function CheckParameters([string]$ipAddress)
     if ($valid -eq $True)
     {
         Write-Host "All parameter check(s) passed." -ForegroundColor Green
+
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
         Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
 
-        exit -1
-    }
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
+        Write-Host ""
 
-    Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-    Write-Host ""
+        break
+    }
 }
 
 function RemovePrinterPort([string]$ipAddress)
