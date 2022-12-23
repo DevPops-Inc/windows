13ceb7bfcdc05bfead27253441e331110305f410
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-22 19:22:32 -0800

    Cleaned spacing in OS check.

--- PowerShell/RemovePrinterPortOnWindows.ps1
+++ PowerShell/RemovePrinterPortOnWindows.ps1
@@ -22,11 +22,11 @@ function CheckOsForWindows()
     else 
     {
         Write-Host "Your operating system is:" $hostOs
-        
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
         Write-Host "Finished checking operating system at" (Get-Date).DateTime
         Write-Host ""
+        
         break
     }
 }
