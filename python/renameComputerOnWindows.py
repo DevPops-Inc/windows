6350f89780c35372a8537e925b6129ee7d1ac044
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-26 08:59:12 -0800

    Upload rename computer script.

---
+++ python/renameComputerOnWindows.py
@@ -0,0 +1,126 @@
+#!/bin/python
+
+# rename computer on Windows
+
+# haven't tested this script on local PC without being on domain yet
+
+# run this script as admin
+# you can run this script: python3 renameComputerOnWindows.py < new name > 
+
+import colorama, os, sys, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
+
+
+def checkOsForWindows():
+    print("Started checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System:", end="")
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+
+        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
+        print("")
+
+    else: 
+        print(Fore.RED + "Sorry but this script only runs on Windows." + Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
+
+        exit("")
+
+
+def getOldName(): 
+    oldName = os.getenv('COMPUTERNAME') 
+    
+    if oldName == None: 
+        oldName = str(input("Please type the computer's current name and press the \"Enter\" key (Example: Victors-PC): "))
+        
+    print("")
+    return oldName
+
+
+def getNewName(): 
+    newName = str(input("Please type the new name and press the \"Enter\" key (Example: Vics-PC): "))
+
+    print("")
+    return newName
+
+
+def checkParameters(oldName, newName): 
+    print("Started checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
+    valid = "true"
+
+    print("Parameter(s):")
+    print("-----------------------------")
+    print("oldName : {0}".format(oldName))
+    print("nameName: {0}".format(newName))
+    print("-----------------------------")
+
+    if oldName == None: 
+        print(Fore.RED + "oldName is not set." + Style.RESET_ALL)
+        valid = "false"
+
+    if newName == None: 
+        print(Fore.RED + "newName is not set." + Style.RESET_ALL)
+        valid = "false"
+
+    if valid == "true": 
+        print(Fore.GREEN + "All parameter check(s) passed." + Style.RESET_ALL)
+
+        print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
+        print("")
+
+    else: 
+        print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
+
+        print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
+        exit("")
+
+
+def renameComputer(): 
+    print("\nRename computer on Windows.\n")
+    checkOsForWindows()
+
+    oldName = getOldName()
+
+    if len(sys.argv) >= 2: 
+        newName = sys.argv[1]
+
+    else: 
+        newName = getNewName()
+
+    checkParameters(oldName, newName)
+
+    try: 
+        startDateTime = datetime.now()
+        print("Started renaming computer at", startDateTime.strftime("%m-%d-%Y %I:%M: %p"))
+
+        print("Your computer's current name is: {0}".format(oldName))
+        
+        renameComputer = "WMIC computersystem where name='{0}' call rename '{1}'".format(oldName, newName) 
+        
+        if os.system(renameComputer) != 0: 
+            raise Exception("Attempt threw an error!")
+        
+        print(Fore.BLUE + "Your computer's current name is: \n")
+        os.getenv('COMPUTERNAME') 
+        print(Fore.GREEN + "Successfully renamed computer." + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+        print("Finished renaming computer at", finishedDateTime.strftime("%m-%d-%Y %I:%M: %p"))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+
+    except Exception as e: 
+        print(Fore.RED + "Failed to rename computer.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+
+renameComputer()
