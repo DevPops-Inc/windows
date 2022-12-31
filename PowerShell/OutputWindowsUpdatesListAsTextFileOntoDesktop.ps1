ead2096758d68551a3e8ff8755b9d20825557b6f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-30 20:07:27 -0800

    Refactored OS check.

--- PowerShell/OutputWindowsUpdatesListAsTextFileOntoDesktop.ps1
+++ PowerShell/OutputWindowsUpdatesListAsTextFileOntoDesktop.ps1
@@ -2,23 +2,26 @@
 
 function CheckOsForWindows()
 {
-    Write-Host "`nChecking operating system..."
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
     $hostOs = [System.Environment]::OSVersion.Platform
 
     if ($hostOs -eq "Win32NT")
     {
-        Write-Host "You are running this script on Windows." -ForegroundColor Green
+        Write-Host "Operating System: " (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
         Write-Host "Your operating system is:" $hostOs
-        
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system.`n"
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        
         break
     }
-    Write-Host "Finished checking operating system.`n"
 }
 
 function OutputWindowsUpdateListOntoDesktop()
