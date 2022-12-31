70d478206bdc278df5ce15ea748215dfcd62e5c0
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-30 19:39:33 -0800

    Revised date time logging in OS check.

--- PowerShell/PingLoopBackIpv4Address.ps1
+++ PowerShell/PingLoopBackIpv4Address.ps1
@@ -15,14 +15,20 @@ function CheckOs()
     if ($hostOs -eq "Win32NT")
     {
         Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
         Write-Host "Operating System:" $hostOs -ForegroundColor Green
+        Write-Host "Sorry but this script only runs on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+
+        break
     }
-    
-    Write-Host "Finished checking operating system at" (Get-Date).DateTime
-    Write-Host ""
 }
 
 function GetLoopbackIpv4Address([System.Net.IPAddress]$loopbackIpv4Address)
