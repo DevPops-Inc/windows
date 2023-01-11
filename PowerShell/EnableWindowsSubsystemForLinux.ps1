c287b0ef54571e5e1ac76db82b64be356c25bd1c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-01-10 20:54:27 -0800

    Removed unknown parameter.

--- PowerShell/EnableWindowsSubsystemForLinux.ps1
+++ PowerShell/EnableWindowsSubsystemForLinux.ps1
@@ -34,7 +34,7 @@ function EnableWsl()
         $startDateTime = (Get-Date)
         Write-Host "Started enabling Windows Subsystem for Linux at" $startDateTime.DateTime
 
-        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -Force
+        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux 
         Write-Host "Successfully enabled Windows Subsystem for Linux." -ForegroundColor Green
 
         $finishedDateTime = (Get-Date)
