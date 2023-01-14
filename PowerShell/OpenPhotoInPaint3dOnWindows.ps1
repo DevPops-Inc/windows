f116c5514af504a5c64b67fa39e5c7f010b8d142
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-01-13 16:43:57 -0800

    Updated date time logging in parameter checks.

--- PowerShell/OpenPhotoInPaint3dOnWindows.ps1
+++ PowerShell/OpenPhotoInPaint3dOnWindows.ps1
@@ -66,16 +66,19 @@ function CheckParameters([string]$fileLocation)
     if ($valid -eq $True)
     {
         Write-Host "All parameter check(s) passed." -ForegroundColor Green
+
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
+        Write-Host ""
     }
     else 
     {
-        Write-Host "One or more parameter checks incorrect, exiting script." -ForegroundColor Red
+        Write-Host "One or more parameter checks incorrect." -ForegroundColor Red
 
-        exit -1
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
+        Write-Host ""
+
+        break
     }
-
-    Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-    Write-Host ""
 }
 
 function OpenPhotoInPaint3d([string]$fileLocation)
