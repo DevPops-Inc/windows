3c4a3b1bb8212f3be20916da8cd97ed0649cfc4c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-02-25 06:58:29 -0800

    Upload Outlook scripts.

---
+++ python/checkOutlookOnWindows.py
@@ -0,0 +1,68 @@
+#!/bin/python 
+
+# check Outlook on Windows 
+
+import colorama, os, sys, traceback
+from colorama import Fore, Style 
+from datetime import datetime
+from pathlib import PureWindowsPath
+colorama.init()
+
+
+def checkOsForWindows(): 
+    print("Started checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System:", end=""); sys.stdout.flush()
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+
+        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+        print("")
+
+    else: 
+        print(Fore.RED + "Sorry but this script only runs on Windows." + Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+        exit("")
+
+
+def checkOutlook():
+    print("\nCheck Outlook on Mac.\n")
+    checkOsForWindows()
+
+    try: 
+        startDateTime = datetime.now()
+        print("Started checking Outlook at", startDateTime.strftime("%m-%d-%Y %I:%M %p"))
+
+        outlookPath = PureWindowsPath("C:/Program Files/Microsoft Office/root/Office16/OUTLOOK.EXE")
+
+        if os.path.exists(outlookPath) == True: 
+            print(Fore.GREEN + "Outlook is installed." + Style.RESET_ALL)
+
+            finishedDateTime = datetime.now()
+            print("Finished checking Outlook at", finishedDateTime.strftime("%m-%d-%Y %I:%M %p"))
+
+            duration = finishedDateTime - startDateTime
+            print("Total execution time: {0} second(s)".format(duration.seconds))
+            print("")
+
+        else: 
+            print(Fore.RED + "Outlook is not installed." + Style.RESET_ALL)
+
+            finishedDateTime = datetime.now()
+            print("Finished checking Outlook at", finishedDateTime.strftime("%m-%d-%Y %I:%M %p"))
+
+            duration = finishedDateTime - startDateTime
+            print("Total execution time: {0} second(s)".format(duration.seconds))
+            exit("")
+
+    except Exception: 
+        print(Fore.RED + "Failed to check Outlook.")
+        traceback.print_exc()
+        exit("" + Style.RESET_ALL)
+
+
+checkOutlook()
---
+++ python/stopAndRelaunchOutlookOnWinOrMac.py
@@ -0,0 +1,126 @@
+#!/bin/python
+
+# stop and relaunch Outlook on Windows or Mac 
+
+import colorama, os, sys, time, traceback
+from colorama import Fore, Style 
+from datetime import datetime
+from pathlib import PureWindowsPath
+colorama.init()
+
+
+def checkOsForWinOrMac(): 
+    print("Started checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System:", end=""); sys.stdout.flush()
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Windows"
+
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System:")
+        os.system('sw_vers')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "macOS"
+
+    else: 
+        print(Fore.RED + "Sorry but this script only runs on Windows or Mac." + Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+        exit("")
+
+    print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+    print("")
+    return operatingSystem
+
+
+def checkOutlook(operatingSystem): 
+    print("Started checking Outlook at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+    if operatingSystem == "Windows": 
+        
+        outlookPath = PureWindowsPath("C:/Program Files/Microsoft Office/root/Office16/OUTLOOK.EXE")
+
+        if os.path.exists(outlookPath) == True: 
+            print(Fore.GREEN + "Outlook is installed." + Style.RESET_ALL)
+
+            print("Finished checking Outlook at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+            print("")
+
+        else: 
+            print(Fore.RED + "Outlook is not installed." + Style.RESET_ALL)
+
+            print("Finished checking Outlook at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+            exit("")
+
+    elif operatingSystem == "macOS": 
+
+        bashOutlookCheck = "open -Ra 'Microsoft Outlook.app'"
+
+        if os.system(bashOutlookCheck) == 0: 
+            print(Fore.GREEN + "Outlook is installed." + Style.RESET_ALL)
+
+            print("Finished checking Outlook at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+            print("")
+
+        else: 
+            print(Fore.RED + "Outlook is not installed." + Style.RESET_ALL)
+
+            print("Finished checking Outlook at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+            exit("")
+
+
+def stopAndRelaunchOutlook(): 
+    print("\nStop and relaunch Outlook on Windows or Mac.\n")
+    
+    operatingSystem = checkOsForWinOrMac()
+    outlookPath = checkOutlook(operatingSystem)
+
+    try: 
+        startDateTime = datetime.now()
+        
+        print("Started stopping and relauching Outlook at", startDateTime.strftime("%m-%d-%Y %I:%M %p"))
+
+        if operatingSystem == "Windows": 
+            outlookApp = 'outlook.exe'
+            stopOutlook = 'taskkill /F /IM {0}'.format(outlookApp)
+            outlookPath = PureWindowsPath("C:/Program Files/Microsoft Office/root/Office16/OUTLOOK.EXE")
+            launchOutlook = 'explorer {0}'.format(outlookPath)
+
+            if os.system(stopOutlook) == 0: 
+                print(Fore.BLUE + "Stopped Outlook and relaunching in 5 seconds.")
+                time.sleep(5)
+
+            os.system(launchOutlook)
+
+        elif operatingSystem == "macOS": 
+            stopOutlook = 'pkill "Microsoft Outlook"'
+            launchOutlook = 'open -a "Microsoft Outlook.app"'
+
+            if os.system(stopOutlook) == 0: 
+                print(Fore.BLUE + "Stopped Outlook and relaunching in 5 seconds.")
+                time.sleep(5)
+
+            if os.system(launchOutlook) != 0:
+                raise Exception("Couldn't relaunch Outlook!")
+            
+        print(Fore.GREEN + "Successfully stopped and relaunched Outlook." + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+
+        print("Finished stopping and relaunching Outlook at", finishedDateTime.strftime("%m-%d-%Y %I:%M %p"))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+
+    except Exception: 
+        print(Fore.RED + "Failed to stop and relaunch Outlook.")
+        traceback.print_exc()
+        exit("" + Style.RESET_ALL)
+
+
+stopAndRelaunchOutlook()
