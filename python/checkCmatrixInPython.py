fdfdba59ee9a327f88b42b3ec51a8830420428e5
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-03-09 05:41:18 -0800

    Refactored script for clarity.

--- python/checkCmatrixInPython.py
+++ python/checkCmatrixInPython.py
@@ -30,7 +30,6 @@ def checkOs():
         operatingSystem = "Linux"
 
     print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
     print("")
     return operatingSystem
 
@@ -41,26 +40,24 @@ def checkCmatrix():
 
     try:
         startDateTime = datetime.now()
-        
         print("Started checking cmatrix at", startDateTime.strftime("%m-%d-%Y %I:%M %p"))
 
         FNULL = open(os.devnull, 'w')
 
         if operatingSystem == "macOS" or operatingSystem == "Linux":
-
             checkCmatrixOnMacOrLinux = subprocess.call(['which', 'cmatrix'], stdout=FNULL) 
 
             if checkCmatrixOnMacOrLinux == 0:
                 print(Fore.GREEN + "cmatrix is installed."+ Style.RESET_ALL)
                 print("")
 
-                print("Do you want to run cmatrix now?")
-                answer = str(input("Please press \"Y\" or \"N\" and press \"return\" key: "))
-                print("")
+                print("Do you want to run cmatrix now?" + Fore.BLUE)
+                answer = str(input("Please press \"Y\" or \"N\" and the \"return\" key: "))
 
                 if answer == "Y":
-                    print("Press \"control\" and \"C\" keys when you're ready to quit cmatrix.")
-                    time.sleep(3)
+                    print("Press the \"control\" and \"C\" keys when you're ready to quit cmatrix." + Style.RESET_ALL)
+
+                    time.sleep(5)
                     os.system('cmatrix')
 
                 print(Fore.GREEN + "Successfully checked cmatrix." + Style.RESET_ALL)
@@ -85,20 +82,19 @@ def checkCmatrix():
                 exit("")
 
         elif operatingSystem == "Windows": 
-            
             checkCmatrixOnWindows = subprocess.call(['where', 'cmatrix'], stdout=FNULL)
 
             if checkCmatrixOnWindows == 0:
                 print(Fore.GREEN + "cmatrix is installed."+ Style.RESET_ALL)
                 print("")
 
-                print("Do you want to run cmatrix now?")
-                answer = str(input("Please press \"Y\" or \"N\" and press \"Enter\" key: "))
-                print("")
+                print("Do you want to run cmatrix now?" + Fore.BLUE)
+                answer = str(input("Please press \"Y\" or \"N\" and the \"Enter\" key: "))
 
                 if answer == "Y":
-                    print("Press \"Ctrl\" and \"C\" keys when you're ready to quit cmatrix.")
-                    time.sleep(3)
+                    print("Press the \"Ctrl\" and \"C\" keys when you're ready to quit cmatrix." + Style.RESET_ALL)
+
+                    time.sleep(5)
                     os.system('cmatrix')
 
                 print(Fore.GREEN + "Successfully checked cmatrix." + Style.RESET_ALL)
@@ -124,7 +120,6 @@ def checkCmatrix():
                 
     except Exception: 
         print(Fore.RED + "Failed to check cmatrix in Python.")
-        
         traceback.print_exc()
         exit("" + Style.RESET_ALL)
 
