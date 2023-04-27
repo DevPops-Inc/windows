4ebb49655f5f7e54f816d2c7440072f99be3a530
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-04-27 06:13:45 -0700

    Cleaned spacing, added checking empty parameter, and improved logging.

--- python/countHeadsOrTailsInCoinFlipsInPython.py
+++ python/countHeadsOrTailsInCoinFlipsInPython.py
@@ -32,9 +32,7 @@ def checkOs():
         operatingSystem = "Linux"
 
     print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
     print("")
-
     return operatingSystem
 
     
@@ -42,19 +40,15 @@ def getCoinFlips(operatingSystem):
     if operatingSystem == "Windows": 
         coinFlip = int(input("Please type the number of coin flips you would like and press \"Enter\" key (Example: 5): "))
 
-        print("")
-
     elif operatingSystem == "macOS" or operatingSystem == "Linux": 
         coinFlip = int(input("Please type the number of coin flips you would like and press \"return\" key (Example: 5): "))
 
-        print("")
-
+    print("")
     return coinFlip
 
 
 def checkParameters(coinFlip): 
     print("Started checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
     valid = True
 
     print("Parameter(s):")
@@ -62,7 +56,7 @@ def checkParameters(coinFlip):
     print("coinFlip: {0}".format(coinFlip))
     print("------------------------------")
 
-    if coinFlip == None: 
+    if coinFlip == None or coinFlip == "": 
         print(Fore.RED + "coinFlip is not set." + Style.RESET_ALL)
         valid = False
     
@@ -70,14 +64,12 @@ def checkParameters(coinFlip):
         print(Fore.GREEN + "All parameter check(s) passed." + Style.RESET_ALL)
 
         print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
         print("")
 
     else: 
         print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
 
         print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
         exit("")
 
     
@@ -98,17 +90,16 @@ def countHeadsOrTailsInCoinFlips():
 
         print("Started counting heads and tails in coin flips at", startDateTime.strftime("%m-%d-%Y %I:%M %p"))
 
-        output={"Heads":0, "Tails":0}
-        coin=list(output.keys())
+        output = {"Heads":0, "Tails":0}
+        coin   = list(output.keys())
 
         for i in range(coinFlip):
             output[random.choice(coin)]+=1
 
-        print("The results of the coin flips are:")
-        print(Fore.BLUE)
+        print("The results of the coin flips are:" + Fore.BLUE)
         print("Heads:", output["Heads"])
         print("Tails:", output["Tails"])
-        print(Style.RESET_ALL)
+        print(Fore.GREEN + "Successfully counted heads and tails in coin flip." + Style.RESET_ALL)
 
         finishedDateTime = datetime.now()
 
@@ -120,7 +111,6 @@ def countHeadsOrTailsInCoinFlips():
         
     except Exception: 
         print(Fore.RED + "Failed to count heads and tails in coin flips.")
-        
         traceback.print_exc()
         exit("" + Style.RESET_ALL)
 
