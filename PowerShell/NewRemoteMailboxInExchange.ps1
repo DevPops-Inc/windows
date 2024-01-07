aee0ecf931dc9ecc9b3588419f2ceeca476f3076
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-07 08:27:00 -0800

    Improved logging in parameter checks.

--- PowerShell/NewRemoteMailboxInExchange.ps1
+++ PowerShell/NewRemoteMailboxInExchange.ps1
@@ -131,15 +131,18 @@ function CheckParameters([string]      $email,
     if ($valid -eq $True)
     {
         Write-Host "All parameter check(s) passed." -ForegroundColor Green
+
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
+        Write-Host ""
     }
     else
     {
         Write-Host "One or more parameter checks incorrect, exiting script." -ForegroundColor Red
 
-        exit -1
+        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
+        Write-Host ""
+        break
     }
-
-    Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
 }
 
 function NewRemoteMailboxInExchange([string]      $email, 
