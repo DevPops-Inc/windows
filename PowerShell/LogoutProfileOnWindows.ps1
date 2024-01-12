c2b0101eeab722c092ef1dc12de158bcae10f085
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-12 05:50:16 -0800

    Cleaned spacing in main function.

--- PowerShell/LogoutProfileOnWindows.ps1
+++ PowerShell/LogoutProfileOnWindows.ps1
@@ -34,18 +34,23 @@ function LogOutProfile()
         Write-Host "Started logging out of profile at" $startDateTime.DateTime
 
         logoff /v
+        Write-Host "Successfully logged out of profile." -ForegroundColor Green
 
         $finishedDateTime = (Get-Date)
         Write-Host "Finished logging out of profile at" $finishedDateTime.DateTime
+
         $duration = New-TimeSpan $startDateTime $finishedDateTime
 
         Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
+
+        Write-Host ""
     }
     catch
     {
         Write-Host "Failed to logout." -ForegroundColor Red
         Write-Host $_ -ForegroundColor Red
         Write-Host $_.ScriptStackTrace -ForegroundColor Red
+        Write-Host ""
     }
 }
 
