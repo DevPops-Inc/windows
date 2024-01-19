0691059c9f143046697a819c69e9835457018031
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-18 17:15:27 -0800

    Cleaned spacing in OS check function.

--- PowerShell/GetUserAccountsOnWindows.ps1
+++ PowerShell/GetUserAccountsOnWindows.ps1
@@ -15,7 +15,6 @@ function CheckOsForWindows()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
         Write-Host "Finished checking operating system."
