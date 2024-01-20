79845a37332edc2cc88ad9d9cd9241ddb01abb3c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-20 06:25:51 -0800

    Added OS check function.

--- PowerShell/GetSystemInfoOnWindows.ps1
+++ PowerShell/GetSystemInfoOnWindows.ps1
@@ -1,5 +1,27 @@
 # get system info on Windows 
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime 
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        Write-Host "Sorry but this script only runs on Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+}
+
 function GetSystemInfo()
 {
     try 
