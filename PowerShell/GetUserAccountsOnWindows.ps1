a411cea17a544d650059fcb0f5b25137e058d93a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-20 06:25:08 -0800

    Improved logging in OS check function.

--- PowerShell/GetUserAccountsOnWindows.ps1
+++ PowerShell/GetUserAccountsOnWindows.ps1
@@ -17,7 +17,7 @@ function CheckOsForWindows()
         Write-Host "Operating System:" $hostOs
         Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
 
-        Write-Host "Finished checking operating system."
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
         Write-Host ""
         break
     }
