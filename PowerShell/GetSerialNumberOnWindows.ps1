9b08fd1bd8b0a1d04afe9179d558898f8620fb3f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-22 05:25:44 -0800

    Cleaned spacing in main function.

--- PowerShell/GetSerialNumberOnWindows.ps1
+++ PowerShell/GetSerialNumberOnWindows.ps1
@@ -35,20 +35,23 @@ function GetSerialNumber()
 
         $serialNumber = Get-CimInstance win32_bios | Format-List serialnumber
         Write-Host "The serial number of this computer is:" $serialNumber
-
         Write-Host "Successfully got serial number." -ForegroundColor Green
         
         $finishedDateTime = (Get-Date)
         Write-Host "Finished getting serial number at" $finishedDateTime.DateTime
+        
         $duration = New-TimeSpan $startDateTime $finishedDateTime
 
         Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
+
+        Write-Host ""
     }
     catch
     {
         Write-Host "Failed to get serial number."  -ForegroundColor Red
         Write-Host $_ -ForegroundColor Red
         Write-Host $_.ScriptStackTrace -ForegroundColor Red
+        Write-Host ""
     }
 }
 
