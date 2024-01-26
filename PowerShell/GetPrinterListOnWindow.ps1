54b7dfd87830f978559feb49b36b9256d16a33af
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-26 06:04:50 -0800

    Improved main function.

--- PowerShell/GetPrinterListOnWindow.ps1
+++ PowerShell/GetPrinterListOnWindow.ps1
@@ -30,14 +30,28 @@ function GetPrinterList()
 
     try 
     {
-        Write-Host "`nThe printers on this computer are: "
+        $startDateTime = (Get-Date)
+        Write-Host "Started getting printer list at" $startDateTime.DateTime
+
+        Write-Host "The printers on this computer are: "
         Get-Printer
-        
-        Write-Host "Successfully got printer list on this computer.`n" -Foreground Green
+        Write-Host "Successfully got printer list on this computer." -Foreground Green
+
+        $finishedDateTime = (Get-Date)
+        Write-Host "Finished getting printer list at" $finishedDateTime.DateTime
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.House, $duration.Minutes, $duration.Seconds)
+
+        Write-Host ""
     }
     catch 
     {
-        Write-Host "`nFailed to get printers on this computer." -ForegroundColor Red
+        Write-Host "Failed to get printers on this computer." -ForegroundColor Red
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
+        Write-Host ""
     }
 }
 
