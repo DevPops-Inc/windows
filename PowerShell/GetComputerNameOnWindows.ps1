a305479a0a815ffc8db89da907eb222226c93510
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-01-30 05:56:00 -0800

    Cleaned spacing in main function.

--- PowerShell/GetComputerNameOnWindows.ps1
+++ PowerShell/GetComputerNameOnWindows.ps1
@@ -34,20 +34,24 @@ function GetComputerName()
         Write-Host "Started getting computer name at" $startDateTime.DateTime
 
         $hostName = $Env:COMPUTERNAME     
-        Write-Host "Successfully got your computer name." -ForegroundColor Green
-        Write-Host "Your computer name is:" $hostName
-
+        Write-Host "The computer name is:" $hostName -ForegroundColor Blue
+        Write-Host "Successfully got computer name." -ForegroundColor Green
+        
         $finishedDateTime = (Get-Date)
         Write-Host "Finished getting computer name at" $finishedDateTime.DateTime
+        
         $duration = New-TimeSpan $startDateTime $finishedDateTime
 
         Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
+
+        Write-Host ""
     }
     catch 
     {
         Write-Host "Failed getting your computer name.`n" -ForegroundColor Red
         Write-Host $_ -ForegroundColor Red
         Write-Host $_.ScriptStackTrace -ForegroundColor Red
+        Write-Host ""
     }
 }
 
