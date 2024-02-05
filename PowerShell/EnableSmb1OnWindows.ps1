046c20f9368667649051b9a23ef69f16839f1230
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-02-05 04:57:44 -0800

    Added OS check function.

--- PowerShell/EnableSmb1OnWindows.ps1
+++ PowerShell/EnableSmb1OnWindows.ps1
@@ -1,5 +1,29 @@
 # enable SMB1 on Windows 
 
+
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        Write-Host "Sorry but this script only runs on Wirndows" -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break 
+    }
+}
+
 function EnableSmb1()
 {
     Write-Host "`nEnable SMB1 on Windows.`n"
