22ac5f42f1fcb3793c16e022f45400b4966b4173
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-02-06 05:55:12 -0800

    Added check OS for Windows function.

--- PowerShell/EnableNetworkAdapterOnWindows.ps1
+++ PowerShell/EnableNetworkAdapterOnWindows.ps1
@@ -7,6 +7,29 @@ param(
     [string] [Parameter(Mandatory = $False)] $networkAdapter = "" # you can set the network adapter here
 )
 
+function CheckOsForWindows()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs
+        Write-Host "Sorry but this script only runs in Windows." -ForegroundColor Red
+
+        Write-Host "Finished checking operating system at" (Get-Date).DateTime
+        Write-Host ""
+        break 
+    }
+}
+
 function GetNetworkAdapter([string]$networkAdapter)
 {
     if (($networkAdapter -eq $Null) -or ($networkAdapter -eq ""))
@@ -27,7 +50,7 @@ function GetNetworkAdapter([string]$networkAdapter)
 
 function EnableNetworkAdapter([string]$networkAdapter)
 {
-    Write-Host "`nEnable network adapter on Windows`n"
+    Write-Host "`nEnable network adapter on Windows.`n"
 
     $networkAdapter = GetNetworkAdapter $networkAdapter
 
