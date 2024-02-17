0b8985c887d22f7eb1524f005346f37ee6ae8c1e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-02-17 07:14:45 -0800

    Replaced break with throw.

--- PowerShell/CheckChromeOnWin.ps1
+++ PowerShell/CheckChromeOnWin.ps1
@@ -20,12 +20,7 @@ function CheckForWindows()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only runs on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        
-        break
+        throw "Sorry but this script only runs on Windows."
     }
 }
 
@@ -105,12 +100,7 @@ function CheckParameters([string]$checkProgram)
     }
     else 
     {
-        Write-Host "One or more parameters are incorrect." -ForegroundColor Red
-        
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-
-        break
+        throw "One or more parameters are incorrect." 
     }
 }
 
