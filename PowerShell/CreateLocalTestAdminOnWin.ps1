a0700eb687ec4e312fad44ecede7f686a3f79a44
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-02-23 06:10:09 -0800

    Replaced break with throw.

--- PowerShell/CreateLocalTestAdminOnWin.ps1
+++ PowerShell/CreateLocalTestAdminOnWin.ps1
@@ -24,12 +24,7 @@ function CheckOsForWindows()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-
-        break
+        throw "Sorry but this script only works on Windows." 
     }
 }
 
@@ -104,12 +99,7 @@ function CheckParameters([string]      $testAdmin,
     }
     else 
     {
-        Write-Host "One or more paramaters are incorrect." -ForegroundColor Red
-
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-
-        break
+        throw "One or more paramaters are incorrect."
     }
 }
 
