71e7cc99b60e9a3ad096b9e5bb01bd403841ec59
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-02-27 05:38:42 -0800

    Replace break with throw.

--- PowerShell/CreateStandardUserOnWin.ps1
+++ PowerShell/CreateStandardUserOnWin.ps1
@@ -26,12 +26,7 @@ function CheckOsForWindows()
   else 
   {
     Write-Host "Operating System:" $hostOs 
-    Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
-
-    Write-Host "Finished checking operating system at" (Get-Date).DateTime
-    Write-Host ""
-
-    break
+    throw "Sorry but this script only works in Windows." 
   }
 }
 
@@ -121,12 +116,7 @@ function CheckParameters([string]      $standardUser,
   }
   else 
   {
-    Write-Host "One or more parameters are incorrect." -ForegroundColor Red
-
-    Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-    Write-Host ""
-
-    break
+    throw "One or more parameters are incorrect." 
   }
 }
 
