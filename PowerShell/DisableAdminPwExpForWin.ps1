553c6dddaaf111e4d582745edcb6abf86676517e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-03-08 05:56:08 -0800

    Replaced break with throw.

--- PowerShell/DisableAdminPwExpForWin.ps1
+++ PowerShell/DisableAdminPwExpForWin.ps1
@@ -22,11 +22,7 @@ function CheckOsForWindows()
     else
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "Sorry but this script only works in Windows."
     }
 }
 
@@ -73,11 +69,7 @@ function CheckParameters([string]$localAdmin)
     }
     else 
     {
-        Write-Host "One or more parameters are incorrect." -ForegroundColor Red
-        
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "One or more parameters are incorrect." 
     }
 }
 
