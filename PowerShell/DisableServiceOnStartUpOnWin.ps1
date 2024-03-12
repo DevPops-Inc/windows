e75b3708b536db14f64bb66884eba9378731fba2
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-03-12 05:52:54 -0700

    Replaced break with throw.

--- PowerShell/DisableServiceOnStartUpOnWin.ps1
+++ PowerShell/DisableServiceOnStartUpOnWin.ps1
@@ -22,11 +22,7 @@ function CheckOsForWindows()
     else
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works in Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "Sorry but this script only works in Windows." 
     }
 }
 
@@ -77,11 +73,7 @@ function CheckParameters([string]$serviceName)
     }
     else 
     {
-        Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
-
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "One or more parameters are incorrect." 
     }
 }
 
