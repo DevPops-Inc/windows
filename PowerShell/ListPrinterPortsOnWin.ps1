ef645d47fa4e3dea8da75919f6759f21c0395a23
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-04-03 06:27:15 -0700

    Replaced break with throw.

--- PowerShell/ListPrinterPortsOnWin.ps1
+++ PowerShell/ListPrinterPortsOnWin.ps1
@@ -15,11 +15,7 @@ function CheckOsForWindows()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "Sorry but this script only works on Windows." 
     }
 }
 
