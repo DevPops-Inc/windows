b581d8b25f9143855610c95e421ba216f626f826
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-04-05 05:47:54 -0700

    Replaced break with throw.

--- PowerShell/GetProgramsOnWin.ps1
+++ PowerShell/GetProgramsOnWin.ps1
@@ -15,11 +15,7 @@ function CheckForWindows()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only runs on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "Sorry but this script only runs on Windows."
     }
 }
 
