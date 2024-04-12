311b4dded388d76a3e81e2bec54b014a6597a17c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-04-12 06:55:13 -0700

    Replaced break with throw.

--- PowerShell/InstallPrinterOnWin.ps1
+++ PowerShell/InstallPrinterOnWin.ps1
@@ -24,11 +24,7 @@ function CheckOsForWin()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "Sorry but this script only works on Windows." 
     }
 }
 
@@ -115,11 +111,7 @@ function CheckParameters([string]$ipAddress, [string]$printerName, [string]$driv
     }
     else 
     {
-        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
-        
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-        break 
+        throw "One or more parameter checks are incorrect, exiting script." 
     }
 }
 
