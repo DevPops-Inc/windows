061bf17942a33769f13c09616f7de078ba07caab
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-04-19 06:06:28 -0700

    Replaced break with throw.

--- PowerShell/MapDriveOnWin.ps1
+++ PowerShell/MapDriveOnWin.ps1
@@ -23,11 +23,7 @@ function CheckOsForWin()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "Sorry but this script only works on Windows." 
     }
 }
 
@@ -91,11 +87,7 @@ function CheckParameters([string]$driveLetter, [string]$path)
     }
     else 
     {
-        Write-Host "One or more parameter checks incorrect, exiting script." -ForegroundColor Red
-
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-        break 
+        throw Write-Host "One or more parameter checks incorrect, exiting script." 
     }
 }
 
