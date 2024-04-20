86a44cb384ade97e86e1ce0a42391fb03e8cc274
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-04-20 06:43:53 -0700

    Replaced break with throw.

--- PowerShell/MoveUserToAdGroup.ps1
+++ PowerShell/MoveUserToAdGroup.ps1
@@ -24,11 +24,7 @@ function CheckOsForWin()
    else 
    {
       Write-Host "Operating System:" $hostOs
-      Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-      Write-Host "Finished checking operating system at" (Get-Date).DateTime
-      Write-Host ""
-      break
+      throw "Sorry but this script only works on Windows." 
    }
 }
 
@@ -113,11 +109,7 @@ function CheckParameters([string]$username, [string]$newGroup, [string]$oldGroup
    }
    else 
    {
-      Write-Host "One or more parameter checks incorrect, exiting script." -ForegroundColor Red
-
-      Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-      Write-Host ""
-      break 
+      throw "One or more parameter checks incorrect, exiting script." 
    }
 }
 
