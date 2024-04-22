db05fcf6973db599bb337266410dea2df183c5d7
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-04-22 05:59:15 -0700

    Replaced break with throw.

--- PowerShell/CreateExchangeContact.ps1
+++ PowerShell/CreateExchangeContact.ps1
@@ -24,11 +24,7 @@ function CheckOsForWin()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "Sorry but this script only works on Windows." 
     }
 }
 
@@ -118,11 +114,7 @@ function CheckParameters([string]$contactName,
     }
     else
     {
-        Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
-
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-        break
+        throw "One or more parameters are incorrect, exiting script." 
     }
 }
 
