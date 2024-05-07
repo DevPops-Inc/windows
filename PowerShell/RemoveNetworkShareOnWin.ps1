ced8d13fd268ed9a0801ec642664be320ed914d9
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-05-07 06:07:44 -0700

    Replaced break with throw.

--- PowerShell/RemoveNetworkShareOnWin.ps1
+++ PowerShell/RemoveNetworkShareOnWin.ps1
@@ -23,13 +23,7 @@ function CheckOsForWin()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-
-        break
+        throw "Sorry but this script only works on Windows." 
     }
 }
 
@@ -73,12 +67,7 @@ function CheckParameters([string]$driveLetter)
     }
     else
     {
-        Write-Host "One or more parameter checks are incorrect, exiting script." -ForegroundColor Red
-
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-
-        break
+        throw "One or more parameter checks are incorrect." 
     }
 }
 
