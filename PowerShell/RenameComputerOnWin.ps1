47d55e1df0cab4da04a90ca779c0c684204e5c54
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-05-11 05:45:19 -0700

    Replaced break with throw.

--- PowerShell/RenameComputerOnWin.ps1
+++ PowerShell/RenameComputerOnWin.ps1
@@ -24,13 +24,7 @@ function CheckOsForWin()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system."
-        Write-Host ""
-
-        break
-    }
+        throw "Sorry but this script only works on Windows."     }
 }
 
 function GetNewName([string]$newName)
@@ -73,12 +67,7 @@ function CheckParameters([string]$newName)
     }
     else 
     {
-        Write-Host "One or more parameter checks are incorrect." -ForegroundColor Red
-
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-
-        break
+        throw "One or more parameter checks are incorrect." 
     }
 }
 
