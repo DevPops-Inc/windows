98db0fc74428d307cf16a7645f28aeea65785634
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-05-24 05:14:27 -0700

    Replaced break with throw.

--- PowerShell/StopAndRelaunchOutlookOnWin.ps1
+++ PowerShell/StopAndRelaunchOutlookOnWin.ps1
@@ -22,12 +22,7 @@ function CheckOsForWin()
     else 
     {
         Write-Host "Operating System:" $hostOs
-        Write-Host "Sorry but this script only works on Windows." -ForegroundColor Red
-
-        Write-Host "Finished checking operating system at" (Get-Date).DateTime
-        Write-Host ""
-
-        break
+        throw "Sorry but this script only works on Windows." 
     }
 }
 
@@ -117,12 +112,7 @@ function CheckParameters([string]$processName,
     }
     else
     {
-        Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
-
-        Write-Host "Finished checking parameter(s) at" (Get-Date).DateTime
-        Write-Host ""
-        
-        break
+        throw "One or more parameters are incorrect."
     }
 }
 
