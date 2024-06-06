d182c769a8acd3154907bd74e0c669b484c16420
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-06-06 06:18:21 -0700

    Cleaned spacing in parameters.

--- PowerShell/ExportSysInfoFileOnWin.ps1
+++ PowerShell/ExportSysInfoFileOnWin.ps1
@@ -4,8 +4,8 @@
 
 [CmdletBinding()]
 param(
-      [string] [Parameter(Mandatory = $False)] $systemInfoFile = "" # you can set system info file here
-    , [string] [Parameter(Mandatory = $False)] $fileDestination = "" # you can set file path here
+    [string] [Parameter(Mandatory = $False)] $systemInfoFile = "", # you can set system info file here
+    [string] [Parameter(Mandatory = $False)] $fileDestination = "" # you can set file path here
 )
 
 function CheckOsForWin()
