05854c0d4a98e575320864ddc51281fa7a82e816
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-08-15 06:40:49 -0700

    Improved exception handling.

--- python/disableUacOnWin.py
+++ python/disableUacOnWin.py
@@ -23,11 +23,7 @@ def checkOsForWindows():
 		print("")
 
 	else: 
-		print(Fore.RED + "Sorry but this script only runs on Windows." + Style.RESET_ALL)
-
-		print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %H:%M %p"))
-
-		exit("")
+		raise Exception("Sorry but this script only runs on Windows.")
 
 
 def disableUserAccessControl(): 
