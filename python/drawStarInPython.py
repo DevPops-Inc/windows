8fa77b8de9290cff7f690849d89b810089590936
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-08-22 05:28:49 -0700

    Improved exception handling.

--- python/drawStarInPython.py
+++ python/drawStarInPython.py
@@ -35,7 +35,7 @@ def checkOs():
         operatingSystem = "Linux"
 
     print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-    
+
     print("")
     return operatingSystem
 
@@ -87,10 +87,7 @@ def checkParameters(borderColor, insideColor):
         print("")
 
     else: 
-        print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
-
-        print("Finished checking parameter(s) at", datetime().strftime("%m-%d-%Y %I:%M %p"))
-        exit("")
+        raise Exception(Fore.RED + "One or more parameters are incorrect.")
 
 
 def drawStar(): 
