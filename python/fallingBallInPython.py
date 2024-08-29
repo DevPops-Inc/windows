5b4105ea774c8c7d107f001988b7870b049a3077
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-08-29 05:45:53 -0700

    Improved exception handling.

--- python/fallingBallInPython.py
+++ python/fallingBallInPython.py
@@ -35,7 +35,7 @@ def checkOs():
         operatingSystem = "Linux"
 
     print("Finished checkig operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-    
+
     print("")
     return operatingSystem
 
@@ -87,10 +87,7 @@ def checkParameters(backgroundColor, fillColor):
         print("")
 
     else: 
-        print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
-
-        print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-        exit("")
+        raise Exception("One or more parameters are incorrect.")
 
 
 def fallingBall(): 
