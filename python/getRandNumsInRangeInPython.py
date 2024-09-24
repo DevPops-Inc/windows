90cb4af8af5c9c36a63a0cb26ad7d203436d346c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-09-24 05:54:13 -0700

    Cleaned spacing.

--- python/getRandNumsInRangeInPython.py
+++ python/getRandNumsInRangeInPython.py
@@ -32,6 +32,7 @@ def checkOs():
         operatingSystem = "Linux"
 
     print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p")) 
+    
     print("")
     return operatingSystem
 
