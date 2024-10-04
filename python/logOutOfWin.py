b7d880192659cfd48fb8561145fb356afbab7468
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-10-04 05:51:27 -0700

    Improved exception handling.

--- python/logOutOfWin.py
+++ python/logOutOfWin.py
@@ -21,9 +21,7 @@ def checkOsForWindows():
         print("")
 
     else: 
-        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
-        exit("")
+        raise Exception("Sorry but this script only runs on Windows.")
 
 
 def logOut(): 
