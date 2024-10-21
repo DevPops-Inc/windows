63a68ab71449a82604d5346445ec84d8bc3515c6
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-10-21 05:52:49 -0700

    Improved exception handling.

--- python/renameComputerOnWin.py
+++ python/renameComputerOnWin.py
@@ -27,11 +27,7 @@ def checkOsForWindows():
         print("")
 
     else: 
-        print(Fore.RED + "Sorry but this script only runs on Windows." + Style.RESET_ALL)
-
-        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
-
-        exit("")
+        raise Exception("Sorry but this script only runs on Windows.")
 
 
 def getOldName(): 
@@ -79,10 +75,7 @@ def checkParameters(oldName, newName):
         print("")
 
     else: 
-        print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
-
-        print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M: %p"))
-        exit("")
+        raise Exception("One or more parameters are incorrect.")
 
 
 def renameComputer(): 
@@ -115,7 +108,7 @@ def renameComputer():
         print(Fore.GREEN + "Successfully renamed computer." + Style.RESET_ALL)
 
         finishedDateTime = datetime.now()
-        
+
         print("Finished renaming computer at", finishedDateTime.strftime("%m-%d-%Y %I:%M: %p"))
 
         duration = finishedDateTime - startDateTime
