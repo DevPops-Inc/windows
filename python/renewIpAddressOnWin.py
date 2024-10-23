57a935c097cac9984328d003182f7a00c925018e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-10-23 06:00:27 -0700

    Cleaned spacing.

--- python/renewIpAddressOnWin.py
+++ python/renewIpAddressOnWin.py
@@ -9,6 +9,7 @@ colorama.init()
 
 
 def checkOsForWindows():
+    
     print("Started checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
 
     if sys.platform == "win32": 
