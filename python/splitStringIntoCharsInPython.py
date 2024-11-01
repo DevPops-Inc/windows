f11d5f962d27c2574fd0c4bce091b6d110982cd2
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-11-01 06:08:14 -0700

    Improved exception handling.

--- python/splitStringIntoCharsInPython.py
+++ python/splitStringIntoCharsInPython.py
@@ -44,7 +44,7 @@ def getStringWithChars(operatingSystem):
         stringWithChars = str(input("Please type a string and press the \"Enter\" key (Example: string): "))
 
     else: 
-        
+
         stringWithChars = str(input("Please type a string and press the \"return\" key (Example: string): "))
 
     print("")
@@ -71,10 +71,7 @@ def checkParameters(stringWithChars):
         print("")
 
     else: 
-        print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
-
-        print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-        exit("")
+        raise Exception("One or more parameters are incorrect.")
 
     
 def splitStringIntoChars(): 
