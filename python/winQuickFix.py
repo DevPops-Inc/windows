5d94f325d2e0898be5e7fac802d54ee6115aeaa6
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-11-12 05:42:29 -0800

    Cleaned spacing.

--- python/winQuickFix.py
+++ python/winQuickFix.py
@@ -11,6 +11,7 @@ colorama.init()
 
 
 def checkOsForWindows(): 
+    
     print("Started checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
 
     if sys.platform == "win32": 
