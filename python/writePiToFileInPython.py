de6fe1e2d31c954a1a06eb037272269e8b22e32a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-11-13 05:36:15 -0800

    Improved exception handling.

--- python/writePiToFileInPython.py
+++ python/writePiToFileInPython.py
@@ -74,11 +74,7 @@ def checkParameters(filename):
 		print("")
 		
 	else: 
-		print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
-		
-		print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
-		exit("")
+		raise Exception("One or more parameters are incorrect.")
 
 
 def checkIfFileExists(operatingSystem, filename): 
