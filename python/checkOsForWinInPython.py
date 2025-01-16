47cd9c3b6778d8298f7a2542e9627a36523312f7
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2025-01-16 06:21:05 -0800

    Added check OS for Windows script.

---
+++ python/checkOsForWinInPython.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+# check OS for Windows
+
+import colorama, os, sys
+from colorama import Fore, Style 
+from datetime import datetime
+colorama.init()
+
+
+def checkOsForWindows(): 
+    print("Started checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
+    if sys.platform == "Windows":
+        print(Fore.GREEN + "Operating System:", end=""); sys.stdout.flush()
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+
+        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+        
+        print("")
+        
+    else:
+        raise Exception("Sorry this script only runs on Windows.")
+
+
+checkOsForWindows()
